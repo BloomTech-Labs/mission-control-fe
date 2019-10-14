b574492dc4548ce0baa0cbfc84484ad1022a1ae9
Dan <danbergelt@protonmail.com>
2019-10-14 18:12:51 -0400

    basic footer/nav styling

---
+++ mission-control-client/src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Nav />
+      <></>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
