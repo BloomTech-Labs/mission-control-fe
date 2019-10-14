c26c37d9c60025dc00401e32dc36a5e930bc75b1
zoverlvx <zoverhulser@gmail.com>
2019-10-14 21:59:52 +0000

    Checks data type of state properties.

---
+++ mission-control-client/src/utils/index.js
@@ -0,0 +1,28 @@
+export function checkType(state, type, i = 0) {
+    // stores the names of properties from state in a string array
+    const properties = Object.keys(state);
+
+    if (i < properties.length) {
+        // if incorrect type, end checking and return false
+        if (typeof(state[properties[i]]) !== type) {
+            return false;
+        }
+        // if correct, continue
+        if (typeof(state[properties[i]]) === type) {
+            i += 1;
+            return checkTypes(state, type, i);
+        }
+    }
+
+    if (i === properties.length) {
+        // if the last is incorrect, end checking
+        if (typeof(state[properties[i]]) !== type) {
+            return false;
+        }
+        // if the last is correct, they're good to go
+        if (typeof(state[properties[i]]) === type) {
+            return true;
+        }
+    }
+}
+        
