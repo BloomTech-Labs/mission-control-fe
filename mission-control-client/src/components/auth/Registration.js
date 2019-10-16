eb53330ea48a1308f75d767bea03fb9b48a667f3
zoverlvx <zoverhulser@gmail.com>
2019-10-16 04:46:43 +0000

    Shows error messages under inputs

--- mission-control-client/src/components/auth/Registration.js
+++ mission-control-client/src/components/auth/Registration.js
@@ -1,209 +1,174 @@
 import React, {useState} from "react";
-import {Button, Form, Message} from "semantic-ui-react";
+import Input from "./Input.js";
+import {Button, Form} from "semantic-ui-react";
 import axios from "axios";
 import validator from "validator";
-import {checkType} from "../../utils";
+
 
 export default function () {
-    
+    let error = false;
     const defaultState = {
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-        confirmedPassword: ""
+        firstName: {
+            type: "text",
+            value: "",
+            error,
+            message: "Please, enter first name."
+        },
+        lastName: {
+            type: "text",
+            value: "",
+            error,
+            message: "Please, enter first name"
+        },
+        email: {
+            type: "email",
+            value: "",
+            error,
+            message: "Please, enter valid email"
+        },
+        password: {
+            type: "password",
+            value: "",
+            error,
+            message: "Please, enter password between 8-16 characters"
+        },
+        confirmPassword: {
+            type: "password",
+            value: "",
+            error,
+            message: "Passwords do not match"
+        } 
     };
 
     const [state, setState] = useState(defaultState);
-    const [error, handleError] = useState({type: "", message: ""});
     
-    const {
-            firstName, 
-            lastName, 
-            email, 
-            password,
-            confirmedPassword
-        } = state;
-
-    function handleChange(e) {
-        setState({
-            ...state,
-            [e.target.name]: e.target.value
-        });
-        console.log("Here is state onChange: ", state)
+    function makeInput(property) {
+        function handleChange(e) {
+            setState({
+                ...state,
+                [property]: {
+                    ...state[property],
+                    value: e.target.value 
+                }
+            });
+        }
+        return (
+            <Input 
+                key={property}
+                name={property}
+                value={state[property].value}
+                label={property}
+                type={state[property].type}
+                error={state[property].error}
+                message={state[property].message}
+                handleChange={handleChange}
+            />        
+        )
     }
+
+    const Inputs = () => Object.keys(state).map(makeInput)
     
     function handleSubmit(e) {
         e.preventDefault();
+        const firstName = state.firstName.value;
+        const lastName = state.lastName.value;
+        const email = state.email.value;
+        const password = state.password.value;
+        const confirmPassword = state.confirmPassword.value;
 
-        const correctType = checkType(state, "string");
-        // if type and lengths are correct
         if (
-           correctType
-           && firstName.length > 0
+           firstName.length > 0
            && lastName.length > 0
            && validator.isEmail(email)
            && password.length >= 8
            && password.length <= 16
-           && confirmedPassword === password
+           && confirmPassword === password
         ) {
-            console.log("Here is everything being submitted: ", state);
+            //TODO: encrypt password
+            const packet = {
+                firstName,
+                lastName,
+                email,
+                password
+            };
+            console.log("Here is everything being submitted: ", packet);
 
-            // no errors exist
-            handleError({
-                ...error,
-                type: "",
-                message: ""
-            });
             // handle input
-            delete state.confirmedPassword;
-            axios.post("get url from Armando", state)
+            axios.post("get url from Armando", packet)
                 .then(res => {
                     console.log(res);
                     setState(defaultState);
                 }).catch(err => console.log(err));
         }
         
-        // if lengths of first or last name are incorrect
-        if (firstName.length <= 0 || lastName.length <= 0) {
-            const type = "NAME";
-            const message = "Please, submit both first name and last name.";
-            handleError({
-                ...error, 
-                type,
-                message 
+        // if length of first name is incorrect
+        error = true;
+        if (firstName.length <= 0) {
+            setState({
+                ...state,
+                firstName: {
+                    ...state.firstName,
+                    error
+                }
+            });
+            return;
+        }
+
+        // if length of last name is incorrect
+        if (lastName.length <= 0) {
+            setState({
+                ...state,
+                lastName: {
+                    ...state.lastName,
+                    error
+                }    
             });
             return;
         }
 
         // if not a valid email
         if (!validator.isEmail(email)) {
-            const type = "EMAIL";
-            const message = "Please, submit a valid email."
-            handleError({
-                ...error, 
-                type,
-                message 
+            setState({
+                ...state,
+                email: {
+                    ...state.email,
+                    error
+                }
             });
             return;
         }
 
         // if length of password is incorrect
         if (password.length < 8 || password.length > 16) {
-            const type = "PASSWORD";
-            const message = "Password must be between 8 and 16 characters";
-            handleError({
-                ...error, 
-                type,
-                message 
+            setState({
+                ...state,
+                password: {
+                    ...state.password,
+                    error
+                }
             });
             return;
         }
         // if confirmed password does not match password
-        if (confirmedPassword !== password) {
-            const type = "CONFIRMED_PASSWORD";
-            const message = "Confirmation does not match password.";
-            handleError({
-                ...error, 
-                type,
-                message 
+        if (confirmPassword !== password) {
+            setState({
+                ...state,
+                confirmPassword: {
+                    ...state.confirmPassword,
+                    error
+                }
             });
-            return;
-        }
-
-        // if data type of any property isn't a string 
-        if (
-           !correctType
-        ) {
-            // handle error
-            alert("Invalid input. Please, fill in all forms.");
             return;
         }
     }
     return (
         <Form onSubmit={handleSubmit}>
-            <Form.Input 
-                name="firstName" 
-                value={firstName} 
-                label="First Name" 
-                type="text" 
-                onChange={handleChange}
-            />
-            
-            <Form.Input 
-                name="lastName" 
-                value={lastName} 
-                label="Last Name" 
-                type="text" 
-                onChange={handleChange}
-            />
             {
-                error.type === "NAME"  ? (
-                    <Message negative>
-                        <Message.Header>
-                            {error.message}
-                        </Message.Header>
-                    </Message>
-                    ) : null
+                Inputs()
             }
-            <Form.Input 
-                name="email" 
-                value={email} 
-                label="Email" 
-                type="text" 
-                onChange={handleChange}
-            />
-            {
-                error.type === "EMAIL"  ? (
-                    <Message negative>
-                        <Message.Header>
-                            {error.message}
-                        </Message.Header>
-                    </Message>
-                    ) : null
-            }
-            <Form.Input 
-                name="password" 
-                value={password} 
-                label="Password" 
-                type="password"
-                onChange={handleChange}
-            />
-            {
-                error.type === "PASSWORD"  ? (
-                    <Message negative>
-                        <Message.Header>
-                            {error.message}
-                        </Message.Header>
-                    </Message>
-                    ) : null
-            }
-            <Form.Input 
-                name="confirmedPassword"
-                value={confirmedPassword}
-                label="Confirm password"
-                type="password"
-                onChange={handleChange}
-            />
-            {
-                error.type === "CONFIRMED_PASSWORD" ? (
-                    <Message negative>
-                        <Message.Header>
-                            {error.message}
-                        </Message.Header>
-                    </Message>
-                ) : null
-            }    
             <Button 
                 type="submit"
                 color="blue"
-                disabled={
-                    !firstName
-                    || !lastName
-                    || !email
-                    || !password
-                    || !confirmedPassword
-                }
             >
                 Submit
             </Button>
