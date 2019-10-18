d46a500b713cc86af16138d63e3ca95bdf15baff
Alex <agordon1123@gmail.com>
2019-10-18 14:02:02 -0400

    WIP - syncing with Armando

--- mission-control-client/src/components/test/Dashboard.js
+++ mission-control-client/src/components/test/Dashboard.js
@@ -1,6 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+// Axios GraphQL Config
+import axiosLabsGraphQL from '../../utils/axiosLabsGraphQL';
+// Pull in query
+import { GET_PRODUCTS, GET_PROJECTS, GET_PERSONS, GET_PERSON, getPerson } from '../../queries';
 
 const Dashboard = ({ children }) => {
+  const [products, setProducts] = useState({});
+  console.log(products);
+
+  useEffect(() => {
+    axiosLabsGraphQL
+      .post('', { query: GET_PRODUCTS })
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    axiosLabsGraphQL
+      .post('', { query: GET_PROJECTS })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    axiosLabsGraphQL
+      .post('', { query: GET_PERSONS })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const id = "ck0ztzki20moz0a30avr3p1oa";
+    axiosLabsGraphQL
+      .post('', { query: GET_PERSON, variables: { id } })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    const id = "ck0ztzki20moz0a30avr3p1oa";
+    console.log(getPerson(id.toString()));
+    axiosLabsGraphQL
+      .post('', { query: getPerson(id) })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
+
   return (
     <>
       <h1>Dashboard</h1>
--- mission-control-client/src/queries/index.js
+++ mission-control-client/src/queries/index.js
@@ -1,68 +1,127 @@
 
-const GET_PRODUCTS = `
+// const GET_PERSON = `
+//     query ($id: ID) {
+//         person(where:{ id: $id }) {
+//             id
+//             firstname
+//             githubId
+//             lastname
+//             program
+//             slackId
+//             timezone
+//         }
+//     }
+// `;
+
+// const getPerson = (id) => `
+//     query {
+//         person(where:{ id: "${id}" }) {
+//             id
+//             firstname
+//             githubId
+//             lastname
+//             program
+//             slackId
+//             timezone
+//         }
+//     }
+// `
+
+const persons = `
     query {
-        products {
-            id
-            productName
+        persons {
+        id
+        firstname
+        githubId
+        lastname
+        program
+        slackId
+        timezone
         }
     }
 `;
 
-const GET_PROJECTS = `
+const person = (id) => `
     query {
-        projects {
-            id
-            name
+        person(where:{id:"${id}"}) {
+        id
+        firstname
+        githubId
+        lastname
+        program
+        slackId
+        timezone
         }
     }
 `;
 
-const GET_PERSONS = `
-query {
-    persons {
-    id
-    firstname
-    githubId
-    lastname
-    program
-    slackId
-    timezone
-    }
-}
-`;
+// Requires email, firstname, githubId, lastname, program, slackId, timezone 
+const createPerson = (params) => `
 
-const GET_PERSON = `
-    query ($id: ID) {
-        person(where:{ id: $id }) {
-            id
-            firstname
-            githubId
-            lastname
-            program
-            slackId
-            timezone
-        }
-    }
 `;
-
-const getPerson = (id) => `
-    query {
-        person(where:{ id: "${id}" }) {
-            id
-            firstname
-            githubId
-            lastname
-            program
-            slackId
-            timezone
-        }
-    }
-`
 
 export {
-    GET_PRODUCTS,
-    GET_PROJECTS,
-    GET_PERSONS,
-    GET_PERSON,
-    getPerson
+    persons,
+    person,
+    createPerson,
+    updatePerson,
+    deletePerson,
+    products,
+    product,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    projectGroups,
+    projectGroup,
+    createProjectGroup,
+    updateProjectGroup,
+    deleteProjectGroup,
+    roles,
+    role,
+    createRole,
+    updateRole,
+    deleteRole,
+    peopleGroups,
+    peopleGroup,
+    createPeopleGroup,
+    updatePeopleGroup,
+    deletePeopleGroup,
+    lambdaRoles,
+    lambdaRole,
+    createLambdaRole,
+    updateLambdaRole,
+    deleteLambdaRole,
+    peopleGroupMembers,
+    peopleGroupMember,
+    createPeopleGroupMember,
+    updatePeopleGroupMember,
+    deletePeopleGroupMember,
+    productRoles,
+    productRole,
+    createProductRole,
+    updateProductRole,
+    deleteProductRole,
+    projects,
+    project,
+    createProject,
+    updateProject,
+    deleteProject,
+    projectGroupMembers,
+    projectGroupMember,
+    createProjectGroupMember,
+    updateProjectGroupMember,
+    deleteProjectGroupMember,
+    projectRoles,
+    projectRole,
+    createProjectRole,
+    updateProjectRole,
+    deleteProjectRole,
+
+    // Created previously
+    createProject,
+    getProducts,
+    getProjects,
+    getPersons,
+    // GET_PERSON,
+    getPerson,
 };
