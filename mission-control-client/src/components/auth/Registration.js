0f36f33bed4c8093b35d10914ca5de4e59447b7e
zoverlvx <zoverhulser@gmail.com>
2019-10-19 23:50:34 +0000

    Restyles with the use of material-ui. Need revamp of sign up design to match request body.

--- mission-control-client/src/components/auth/Registration.js
+++ mission-control-client/src/components/auth/Registration.js
@@ -1,62 +1,123 @@
 import React from "react";
 import axios from "axios";
-import {Form, Field, withFormik} from "formik";
+import {withFormik} from "formik";
+import {withStyles} from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 import {useHistory} from "react-router-dom";
 import * as Yup from "yup";
 const URL = "http://mission-control-be-dev.us-east-1.elasticbeanstalk.com/api/auth/admin/register";
 // TODO: encrypt password
 
-function RegistrationForm({
+const styles = () => ({
+    container: {
+        position: "absolute",
+        width: "1760px",
+        height: "897px",
+        left: "80px",
+        top: "176px",
+        background: "#FFFFFF",
+        borderRadius: "5px"
+    },
+    header: {
+        position: "absolute",
+        width: "136px",
+        height: "43px",
+        left: "217.5px",
+        top: "313.5px",
+        fontFamily: "Lato",
+        fontStyle: "normal",
+        fontWeight: "500",
+        fontSize: "36px",
+        lineHeight: "43px",
+        textAlign: "center",
+        letterSpacing: "0.06em",
+        color: "#0051BE"
+    }
+});
+function Form({
+    classes,
     errors,
     touched,
     values,
-    handleSubmit,
-    status
+    handleSubmit
 }) {
     const history = useHistory();
     return (
-        <Form
-            history={history}
-        >
-            <Field 
-                type="text"
-                name="firstName"
-            />
-            {touched.firstName && errors.firstName && (
-                <p className="error">{errors.firstName}</p>
-            )}
-            <Field 
-                type="text"
-                name="lastName"
-            />
-            {touched.lastName && errors.lastName && (
-                <p className="error">{errors.lastName}</p>
-            )}
-            <Field 
-                type="email"
-                name="email"
-            />
-            {touched.email && errors.email && (
-                <p className="error">{errors.email}</p>
-            )}
-            <Field 
-                type="password"
-                name="password"
-            />
-            {touched.password && errors.password && (
-                <p className="error">{errors.password}</p>
-            )}
-            <Field 
-                type="password"
-                name="confirmPassword"
-            />
-            {touched.confirmPassword && errors.password && (
-                <p className="error">{errors.confirmPassword}</p>
-            )}
-            <button type="submit">Submit</button>
-        </Form>      
+        <div className={classes.container}>
+            <h1 
+                className={classes.header}
+            >
+                Sign Up
+            </h1>
+            <p>Already have an account? Sign In</p>
+            <form
+                history={history}
+                onSubmit={handleSubmit}
+            >
+                <Card>
+                    <CardContent>
+                        <TextField 
+                            type="text"
+                            id="firstName"
+                            name="firstName"
+                            label="First Name"
+                        />
+                        {touched.firstName && errors.firstName && (
+                            <p className="error">{errors.firstName}</p>
+                        )}
+                        <TextField 
+                            type="text"
+                            id="firstName"
+                            name="lastName"
+                            label="Last Name"
+                        />
+                        {touched.lastName && errors.lastName && (
+                            <p className="error">{errors.lastName}</p>
+                        )}
+                        <TextField 
+                            type="email"
+                            id="email"
+                            name="email"
+                            label="Email"
+                        />
+                        {touched.email && errors.email && (
+                            <p className="error">{errors.email}</p>
+                        )}
+                        <TextField 
+                            type="password"
+                            id="password"
+                            name="password"
+                            label="Password"
+                        />
+                        {touched.password && errors.password && (
+                            <p className="error">{errors.password}</p>
+                        )}
+                        <TextField 
+                            type="password"
+                            id="confirmPassword"
+                            name="confirmPassword"
+                            label="Confirm Password"
+                        />
+                        {touched.confirmPassword && errors.password && (
+                            <p className="error">{errors.confirmPassword}</p>
+                        )}
+                        <Button 
+                            color="primary" 
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                    </CardContent>
+                </Card>
+            </form>
+        </div>
     );
 }
+
+const RegistrationForm = withStyles(styles)(Form);
 
 export default withFormik({
     mapPropsToValues({
@@ -76,16 +137,19 @@ export default withFormik({
         };
     },
     validationSchema: Yup.object().shape({
-        firstName: Yup.string().required("Please, enter first name."),
-        lastName: Yup.string().required("Please, enter last name."),
-        email: Yup.string().email("Invalid email").required("Please, enter a valid email"),
+        firstName: Yup.string()
+            .required("Please, enter first name."),
+        lastName: Yup.string()
+            .required("Please, enter last name."),
+        email: Yup.string().email("Invalid email")
+            .required("Please, enter a valid email"),
         password: Yup.string()
             .min(8, "Password must be at least 8 characters.")
             .max(16, "Password cannot be more than 16 characters.")
             .required("Please, enter a password between 8 and 16 characters."),
         confirmPassword: Yup.string()
-            .oneOf([Yup.ref("password")], "Passwords must match.").required("Please, confirm password")
-        
+            .oneOf([Yup.ref("password")], "Passwords must match.")
+            .required("Please, confirm password")
     }),
     handleSubmit(values, {setStatus, history}) {
         const packet = {
@@ -94,7 +158,7 @@ export default withFormik({
             email: values.email,
             password: values.password,
             roleId: "abc123"
-        }
+        };
         axios.post(URL, packet)
             .then(res => {
                 localStorage.setItem("token", res.data.token);
