4cbf2ec66ff89024b0a8da756c9c13e84006a88c
zoverlvx <zoverhulser@gmail.com>
2019-10-21 20:10:24 +0000

    Removes footer

--- mission-control-client/src/components/layout/Layout.js
+++ mission-control-client/src/components/layout/Layout.js
@@ -2,13 +2,16 @@ import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
+const style = {
+    backgroundColor: "#E5E5E5"
+};
+
 const Layout = ({ children }) => {
   return (
-    <>
+    <div style={style}>
       <Nav />
       <>{children}</>
-      <Footer />
-    </>
+    </div>
   );
 };
 
