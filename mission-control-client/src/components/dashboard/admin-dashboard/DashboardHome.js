33b66651d5b45b97edf9db88c91137a9aac99749
Dan <danbergelt@protonmail.com>
2019-10-22 12:55:35 -0400

    expandable banner + charts svg

--- mission-control-client/src/components/dashboard/admin-dashboard/DashboardHome.js
+++ mission-control-client/src/components/dashboard/admin-dashboard/DashboardHome.js
@@ -1,35 +1,15 @@
-import React, { useEffect } from "react";
+import React from "react";
 import ProductList from "./ProductList";
 import DashboardContent from "./DashboardContent";
-import { getProducts } from "../../../actions/productActions";
-import { getProjectGroups } from '../../../actions/projectActions';
-import { connect } from "react-redux";
 
-const DashboardHome = ({ getProducts, getProjectGroups, products }) => {
-
-  useEffect(() => {
-    getProducts();
-    getProjectGroups();
-  }, [])
+const DashboardHome = () => {
 
   return (
     <div className="admin-dashboard-container">
       <ProductList />
       <DashboardContent />
-      {products && products.map(el => (
-        <p key={el.id}>{el.productName}</p>
-      ))}
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    products: state.productStore.products
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  { getProducts, getProjectGroups }
-)(DashboardHome);
+export default DashboardHome;
