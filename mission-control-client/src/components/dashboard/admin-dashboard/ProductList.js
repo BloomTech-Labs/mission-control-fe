e38db03c457ad1ad1ab72426d56e322af60d72a5
Alex <agordon1123@gmail.com>
2019-10-23 10:08:15 -0400

    created product search handler

--- mission-control-client/src/components/dashboard/admin-dashboard/ProductList.js
+++ mission-control-client/src/components/dashboard/admin-dashboard/ProductList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
@@ -9,6 +9,26 @@ import Product from "./Product";
 import res from "../../../data/projects";
 
 const ProductList = () => {
+  // Create a state object to hold the projects array
+  // This data will most likely be passed down as props from DashboardHome
+  const [filtered, setFiltered] = useState({ items: res.data.projects });
+
+  const handleChange = e => {
+    // If the search param is not an empty string and our data array has length(matchers)
+    if (e.target.value !== "" && filtered.items.length) {
+      let items = filtered.items;
+      // Filter through the items and return each item.name that matches the search params
+      items = items.filter(item => {
+        return item.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+      })
+      // Then set the filtered array to state
+      setFiltered({ items: items });
+    } else {
+      // If the search params are an empty string and the data array has no matchers, reset array
+      setFiltered({ items: res.data.projects });
+    }
+  };
+
   return (
     <div className="product-list-container">
         <div className="product-list-header">
@@ -18,6 +38,7 @@ const ProductList = () => {
         <TextField
           variant="outlined"
           placeholder="Search here"
+          onChange={handleChange}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
@@ -27,7 +48,7 @@ const ProductList = () => {
           }}
         />
         <div className="products-scroll-container">
-          {res.data.projects.map((el, i) => (
+          {filtered.items.map((el, i) => (
             <Product key={i}/>
           ))}
         </div>
