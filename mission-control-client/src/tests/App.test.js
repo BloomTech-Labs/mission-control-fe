f1b6d0e07558f528d2f2ef0923bbd69351c6c359
Dan <danbergelt@protonmail.com>
2019-10-23 19:22:00 -0400

    more tests added for logging in

--- mission-control-client/src/tests/App.test.js
+++ mission-control-client/src/tests/App.test.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { Router } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
+import React from "react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 
-import App from '../App';
-import * as rtl from '@testing-library/react';
+import App from "../App";
+import * as rtl from "@testing-library/react";
 afterEach(rtl.cleanup);
 
-describe('App', () => {
-    it ('renders without crashing', () => {
-        const history = createMemoryHistory();
-        const wrapper = rtl.render(
-            <Router history={history}>
-                <App />
-            </Router>);
-        expect(wrapper.baseElement).toMatchSnapshot();
-    });
+describe("App", () => {
+  it("renders without crashing", () => {
+    const history = createMemoryHistory();
+    const wrapper = rtl.render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
+    expect(wrapper.baseElement).toMatchSnapshot();
+  });
 });
