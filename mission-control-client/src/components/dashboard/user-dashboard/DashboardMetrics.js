76fa5d680dec7cf099a5fad421d494447f144338
Alex <agordon1123@gmail.com>
2019-10-24 18:54:31 -0400

    created tab component to display users or overview

--- mission-control-client/src/components/dashboard/user-dashboard/DashboardMetrics.js
+++ mission-control-client/src/components/dashboard/user-dashboard/DashboardMetrics.js
@@ -1,10 +1,59 @@
-import React from 'react';
-import { ReactComponent as MockDash } from '../../../assets/mockdash.svg'
+import React, { useState } from 'react';
+import { Tabs, AppBar, Tab, Typography, Box } from '@material-ui/core'
+
+// ToDo: connect to redux store and getPeople connected to this project
+// ToDo: Create a sub-component called tabs that will take in the tab info and also hold
+//        the teammate cards
+
+const TabPanel = props => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      // id={`full-width-tabpanel-${index}`}
+      // aria-labelledby={`full-width-tab-${index}`}
+      // {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
 
 const DashboardMetrics = () => {
+  // Set value for tab content
+  const [value, setValue] = useState(0);
+  console.log(value);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    // <MockDash style={{width: '100%'}}/>
-    <></>
+    <div className='dashboard-metrics-container' style={{ marginTop: '50px' }}>
+      <AppBar position='static' color='default'>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="inherit"
+          variant="fullWidth"
+          // aria-label="user dashboard metrics tab bar"
+        >
+          <Tab label="Users" />
+          <Tab label="Overview" />
+        </Tabs>
+
+        <TabPanel value={value} index={0}>
+          Users
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Overview
+        </TabPanel>
+      </AppBar>
+    </div>
   )
 }
 
