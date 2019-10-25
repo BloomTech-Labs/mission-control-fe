610a0e9175f1e45d95ece08b5f2ac1503ca03b37
Alex <agordon1123@gmail.com>
2019-10-24 23:15:13 -0400

    created single user tab and integrated github api for image

--- mission-control-client/src/components/dashboard/user-dashboard/DashboardMetrics.js
+++ mission-control-client/src/components/dashboard/user-dashboard/DashboardMetrics.js
@@ -1,60 +1,56 @@
-import React, { useState } from 'react';
-import { Tabs, AppBar, Tab, Typography, Box } from '@material-ui/core'
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { getProjectGroupMembers } from '../../../actions';
+import TabPanel from './TabPanel';
+import { Tabs, AppBar, Tab } from '@material-ui/core'
+import UsersTab from './UsersTab';
+import OverviewTab from './OverviewTab';
 
-// ToDo: connect to redux store and getPeople connected to this project
-// ToDo: Create a sub-component called tabs that will take in the tab info and also hold
-//        the teammate cards
+import res from '../../../data/projectGroupMemberData';
 
-const TabPanel = props => {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      // id={`full-width-tabpanel-${index}`}
-      // aria-labelledby={`full-width-tab-${index}`}
-      // {...other}
-    >
-      <Box p={3}>{children}</Box>
-    </Typography>
-  );
-}
-
-const DashboardMetrics = () => {
-  // Set value for tab content
+const DashboardMetrics = props => {
+  console.log(props);
   const [value, setValue] = useState(0);
-  console.log(value);
+
+  useEffect(() => {
+    // getProjectGroupMembers();
+    // API call returns an error
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <div className='dashboard-metrics-container' style={{ marginTop: '50px' }}>
-      <AppBar position='static' color='default'>
+    <div className='dashboard-metrics-container' style={{ backgroundColor: '#E7EBFF', marginTop: '50px' }}>
+      <AppBar position='static' color='default' style={{ boxShadow: 'none' }}>
         <Tabs
           value={value}
           onChange={handleChange}
-          indicatorColor="primary"
-          textColor="inherit"
-          variant="fullWidth"
-          // aria-label="user dashboard metrics tab bar"
+          indicatorColor='primary'
+          textColor='inherit'
+          variant='fullWidth'
+          style={{ backgroundColor: 'white' }}
         >
-          <Tab label="Users" />
-          <Tab label="Overview" />
+          <Tab label='Users' />
+          <Tab label='Overview' />
         </Tabs>
 
         <TabPanel value={value} index={0}>
-          Users
+          <UsersTab props={res.data.projectGroupMembers} />
         </TabPanel>
         <TabPanel value={value} index={1}>
-          Overview
+          <OverviewTab />
         </TabPanel>
       </AppBar>
     </div>
   )
+};
+
+const mapStateToProps = state => {
+  return {
+    projectStore: state.projectStore
+  }
 }
 
-export default DashboardMetrics;
+export default connect(mapStateToProps, getProjectGroupMembers)(DashboardMetrics);
