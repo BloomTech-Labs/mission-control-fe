9b2cca028ee9c966caf4ddea4fabce6cc3aef38b
Dan <danbergelt@protonmail.com>
2019-10-28 15:30:42 -0400

    404 page implemented

---
+++ mission-control-client/src/components/layout/Bad.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Bad = () => {
+  return (
+    <div className="bad-container">
+      <p className="bad-header">404</p>
+      <p className="bad-desc">This page does not exist</p>
+    </div>
+  );
+};
+
+export default Bad;
