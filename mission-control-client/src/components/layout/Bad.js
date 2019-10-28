fcf311cc8f22554ec0629e494fc35605ebae2930
Dan <danbergelt@protonmail.com>
2019-10-28 19:55:37 -0400

    redirects testing done

--- mission-control-client/src/components/layout/Bad.js
+++ mission-control-client/src/components/layout/Bad.js
@@ -3,7 +3,7 @@ import React from "react";
 const Bad = () => {
   return (
     <div className="bad-container">
-      <p className="bad-header">404</p>
+      <p data-testid="bad" className="bad-header">404</p>
       <p className="bad-desc">This page does not exist</p>
     </div>
   );
