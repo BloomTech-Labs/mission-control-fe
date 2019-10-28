678a56c723dd91962d8d7a9301501823a244c782
Dan <danbergelt@protonmail.com>
2019-10-28 18:48:58 -0400

    home path redirects

--- mission-control-client/src/App.js
+++ mission-control-client/src/App.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
-import { Switch, Route, useLocation } from "react-router-dom";
+import { Switch, Route, useLocation, Redirect } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Layout from "./components/layout/Layout";
 import "./styles/index.scss";
 import Registration from "./components/auth/Registration.js";
 import Login from './components/auth/Login';
-import Home from "./components/test/Home";
 import Bad from './components/layout/Bad';
 import DashboardHome from "./components/dashboard/admin-dashboard/DashboardHome";
 import embedAnalytics from './utils/embedAnalytics';
@@ -20,7 +19,7 @@ function App() {
   return (
     <Layout>
       <Switch>
-        <PrivateRoute path="/" exact component={DashboardHome} />
+        <PrivateRoute path="/" exact>{localStorage.getItem('token')? <Redirect to={{pathname: `/dashboard/${localStorage.getItem('user')}`}}/> : <Redirect to="/login" />}</PrivateRoute>
         <Route path="/register" component={Registration} />
         <Route path="/login" component={Login} />
         <PrivateRoute path="/dashboard/:id" component={DashboardHome} />
