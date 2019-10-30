9c1a8959595cb21e2c14a86d021f0909345d2366
Armando Roman <armandoroman1016@gmail.com>
2019-10-30 14:26:16 -0700

    styles added to update forms

--- mission-control-client/src/components/user_settings/EditForm.js
+++ mission-control-client/src/components/user_settings/EditForm.js
@@ -4,20 +4,43 @@ import { connect } from 'react-redux'
 import * as Yup from 'yup'
 
 const FormShape = props => {
+
+    const { params } = props
+
+    const { location } = props
+
+    let formType = location.pathname.split('/')
+
+    formType = formType[formType.length - 1]
+
+    console.log(formType)
+
+    const isPassword = formType === 'password'
+
     return(
-        <div>
-            <Form>
-                <h3>Edit Password</h3>
-                <Field name = 'password' type = 'password' placeholder = 'Password'/>
+        <div className = 'update-form-container'>
+        <Form className = 'update-form'>
+            <h3>Update {formType}</h3>
+            <div className = 'inputs'>
+                <Field name = {formType} type = { isPassword ? 'password' : 'email'} placeholder = { isPassword ? "Current Password" : "Current Email"} className = 'current'/>
+                <Field name = {`new${formType}`} type = { isPassword ? 'password' : 'email' } placeholder = { isPassword ? "New Password" : "New Email" } className = 'new'/>
+                <Field name = {`confirm${formType}`} type = { isPassword ? 'password' : 'email' } placeholder = { isPassword ? "Confirm New Password" : "Confirm New Email"} className = 'confirm'/>
+            </div>
+                <button type = 'submit'>CONFIRM CHANGES</button>
             </Form>        
         </div>
     )
 }
 
 const EditForm = withFormik({
-    mapPropsToValues({password}){
+    mapPropsToValues({password, email, newemail, newpassword, confirmemail, confirmpassword}){
         return{
+            email: email || '',
+            newemail: newemail || '',
+            confirmemail: confirmemail || '',
             password: password || '',
+            newpassword: newpassword || '',
+            confirmpassword: confirmpassword || ''
         }    
     },
     validationSchema: Yup.object().shape({
