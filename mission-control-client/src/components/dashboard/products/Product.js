70d8a73cbea80e4fb9ccd85a16bd6eebb56bbfae
Dan <danbergelt@protonmail.com>
2019-11-06 18:03:20 -0500

    cleaning up bad content

--- mission-control-client/src/components/dashboard/products/Product.js
+++ mission-control-client/src/components/dashboard/products/Product.js
@@ -14,31 +14,6 @@ const Product = props => {
     >
       <div className="product-description">
         <h3 className="product-title">{props.el.name}</h3>
-        {props.i.split('').pop() % 2 === 0 ? (
-          <p className="product-status-completed">Completed</p>
-        ) : (
-          <p className="product-status-not-completed">Not Completed</p>
-        )}
-      </div>
-      <div className="product-programs">
-        {programs.map(
-          (el, i) =>
-            (el === "ux/ui" && (
-              <p key={i} className="product-program-avatar program-ux">
-                {el.toUpperCase()}
-              </p>
-            )) ||
-            (el === "ds" && (
-              <p key={i} className="product-program-avatar program-ds">
-                {el.toUpperCase()}
-              </p>
-            )) ||
-            (el === "web" && (
-              <p key={i} className="product-program-avatar program-web">
-                {el.toUpperCase()}
-              </p>
-            ))
-        )}
       </div>
     </div>
   );
