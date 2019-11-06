0f8af7abd4cb3efb0343574bd45f3c9e5550dc31
zoverlvx <zoverhulser@gmail.com>
2019-11-06 23:17:36 +0000

    Outlines functional architecture of component.

---
+++ mission-control-client/src/components/dashboard/user-dashboard/ProjectDetails.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {connect} from "react-redux";
+
+function ProjectDetails(props) {
+    return (
+        <div>
+        </div>      
+    );
+}
+
+function mapStateToProps(state) {
+    return state;
+}
+
+export default connect(mapStateToProps, {})(ProjectDetails);
