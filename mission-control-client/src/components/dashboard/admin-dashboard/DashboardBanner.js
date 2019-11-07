4584e660fbc2edd65d9178487725819aec8ebb4e
Dan <danbergelt@protonmail.com>
2019-11-07 15:50:41 -0500

    rendering team data + project data on project page

--- mission-control-client/src/components/dashboard/admin-dashboard/DashboardBanner.js
+++ mission-control-client/src/components/dashboard/admin-dashboard/DashboardBanner.js
@@ -8,29 +8,6 @@ const DashboardBanner = props => {
     <div className="dashboard-banner-container">
       <div className="dashboard-banner-head">
           <p>{props.activeProductStore.active ? props.activeProductStore.active.name : "Loading..."}</p>
-          <div className="dashboard-product-project-programs">
-            {/* {programs.map(
-              (el, i) =>
-                (el === "ux/ui" && (
-                  <p key={i} className="product-program-avatar program-ux">
-                    {el.toUpperCase()}
-                  </p>
-                )) ||
-                (el === "ds" && (
-                  <p key={i} className="product-program-avatar program-ds">
-                    {el.toUpperCase()}
-                  </p>
-                )) ||
-                (el === "web" && (
-                  <p key={i} className="product-program-avatar program-web">
-                    {el.toUpperCase()}
-                  </p>
-                ))
-            )} */}
-          </div>
-        {/* <div className="dashboard-product-status">
-          <p>In Progress</p>
-        </div> */}
       </div>
       <div className="dashboard-product-projects">
         <p className="dashboard-product-total-projects">Total Projects: {props.activeProductStore.active && props.activeProductStore.active.projects.length}</p>
