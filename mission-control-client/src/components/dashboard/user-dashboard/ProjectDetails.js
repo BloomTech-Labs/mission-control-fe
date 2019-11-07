d337355be7b6cb2873de2c63f613192559e63a77
zoverlvx <zoverhulser@gmail.com>
2019-11-07 00:09:02 +0000

    Musing

--- mission-control-client/src/components/dashboard/user-dashboard/ProjectDetails.js
+++ mission-control-client/src/components/dashboard/user-dashboard/ProjectDetails.js
@@ -1,9 +1,24 @@
 import React from "react";
 import {connect} from "react-redux";
 
+// project name
+// project start date
+// project end date
+// team lead
+// section lead
+// engineering manager
+// ux manager
+// developers
+/*
+    Received from GET:
+    {
+        
+    }
+*/
 function ProjectDetails(props) {
     return (
         <div>
+            {props.name}
         </div>      
     );
 }
