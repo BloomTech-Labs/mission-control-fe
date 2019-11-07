2c5a7aa159137ee249a017cf6c65e259d6bad902
zoverlvx <zoverhulser@gmail.com>
2019-11-07 21:26:27 +0000

    Fixes typo

--- mission-control-client/src/reducers/index.js
+++ mission-control-client/src/reducers/index.js
@@ -5,7 +5,7 @@ import { personStore } from './personStore'
 import { productStore } from './productStore'
 import { projectStore } from './projectStore'
 import { activeProductStore } from './activeProductStore';
-import { activeProjetStore } from "./activeProjectStore";
+import { activeProjectStore } from "./activeProjectStore";
 
 export default combineReducers({
     globalStore, 
