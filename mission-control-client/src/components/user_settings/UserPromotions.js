721041a7683f2b6ef6c958728c526f69632575e1
Armando Roman <armandoroman1016@gmail.com>
2019-11-07 16:54:59 -0800

    modal added

--- mission-control-client/src/components/user_settings/UserPromotions.js
+++ mission-control-client/src/components/user_settings/UserPromotions.js
@@ -1,37 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { connect } from 'react-redux'
 import { getUsers } from '../../actions'
 import { css } from '@emotion/core';
 import ClipLoader from 'react-spinners/ClipLoader';
-
+import UserModal from './UserPromotionModal'
 
 
 const UserPromotions = props => {
 
     const { users } = props
-    
+
+    const [ className, setClassName ] = useState('hide')
+
     useEffect(() => {
         if(!users.length){
             props.getUsers();
         }
     },[])
+    
+    const container = useRef(null)
 
-    const updateRole = () => {
-        console.log('hello')
-    }
+    // const hideBtn = (e) => {
+    //     console.log(e.target)
+    //     setClassName('hide')
+    // }
+
+    // const showBtn = (e) => {
+    //     console.log(e.target)
+    //     setClassName('')
+    // }
 
     return (
         <div className = 'roles-container'>
             <h2>Promote User Roles</h2>
-            <div className = 'users-container'>
+            <div className = 'users-container '>
                 { !props.isLoading ? 
                     users.map( user => { 
                         return (
-                            <div className = 'user' key = {user.userId}>
+                            <div 
+                            className = 'user' 
+                            key = {user.userId} 
+                            // onMouseEnter = {(e) => showBtn(e)} 
+                            // onMouseLeave = {(e) => hideBtn(e)} 
+                            ref = {container} >
                                 <h3>{`${user.firstName} ${user.lastName}`}</h3>
                                 <span>-</span>
                                 <p>{user.role.charAt(0).toUpperCase() + user.role.substring(1)}</p>
-                                <button onClick={updateRole}>Update Role</button>
+                                <UserModal user = {user} className = {`${className}`} />
                             </div>)
                     }) 
                 : <ClipLoader
