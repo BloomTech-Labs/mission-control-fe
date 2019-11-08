ad838a342c12cbdbcb12e08ac7cb6b4c998f4d99
Alex <agordon1123@gmail.com>
2019-11-08 14:59:43 -0500

    WIP

--- mission-control-client/src/actions/activeProjectActions.js
+++ mission-control-client/src/actions/activeProjectActions.js
@@ -1,7 +1,21 @@
+import axios from 'axios';
+import { peopleByProjectId } from '../queries';
+
 export const SET_ACTIVE_PROJECT = "SET_ACTIVE_PROJECT";
+export const SET_ACTIVE_PROJECT_SUCCESS = 'SET_ACTIVE_PROJECT_SUCCESS';
+export const SET_ACTIVE_PROJECT_FAILURE = 'SET_ACTIVE_PROJECT_FAILURE';
 
 export const setActiveProject = el => {
   return dispatch => {
-    dispatch({type: SET_ACTIVE_PROJECT, payload: el})
+    axios
+      .post("", { query: peopleByProjectId(el.id) })
+      .then(res =>{
+        console.log(res);
+        const project = res.data;
+        dispatch({ type: SET_ACTIVE_PROJECT_SUCCESS, payload: project });
+      })
+      .catch(err => {
+        dispatch({ type: SET_ACTIVE_PROJECT_FAILURE, payload: err.response });
+      });
   }
 }
