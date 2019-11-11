842af20fcb69dcf59b56ff9832a266c78bbb0a61
zoverlvx <zoverhulser@gmail.com>
2019-11-11 22:23:49 +0000

    Applies data to component.

--- mission-control-client/src/components/dashboard/projects/ProjectDetails.js
+++ mission-control-client/src/components/dashboard/projects/ProjectDetails.js
@@ -16,13 +16,30 @@ import {connect} from "react-redux";
     }
 */
 function ProjectDetails(props) {
-    const { activeProjectStore: { active } } = props;
-    console.log("Line 20: here is active: ", active);
+    const { active } = props.activeProjectStore;
     
     if(active) {
+        const { 
+            people, 
+            project: {
+                person,
+                role,
+                project // should be changed to projectDetails
+            }
+        } = active;
+    const projectRoleOfUser = `${person.firstname} ${person.lastname}'s ${role.type}: ${role.name}`; // should be camel-cased
+
+    console.log("user role: ", projectRoleOfUser);
+    console.log("Line 33: here is people: ", people);
+    console.log("Line 34: here is person: ", person);
+    console.log("Line 35: here is role: ", role);
+    console.log("Line 36: here is project: ", project);
+        
         return (
             <div>
-                Details loaded
+                <h1>Product Name: {project.product.name}</h1>
+                <h2>Cohort: {project.name}</h2>
+                <h3>{projectRoleOfUser}</h3>
             </div>       
         );
     }
