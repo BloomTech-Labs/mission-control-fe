20d4bd037ce1dc8fe5225e7992a88c1126c38a05
Dan Bergelt <50968952+danbergelt@users.noreply.github.com>
2019-11-11 11:52:51 -0500

    Update axiosLabsGraphQL.js
    Updating API token

--- mission-control-client/src/utils/axiosLabsGraphQL.js
+++ mission-control-client/src/utils/axiosLabsGraphQL.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosLabsGraphQL = axios.create({
   baseURL: "https://api.use-mission-control.com/",
   headers: {
-     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTcyODgzOTUxLCJleHAiOjE1NzM0ODg3NTF9.9QKq5sKaQLlOVzw5vOAlfnC_4W7M34VGAz9jdlV1fD0" 
+     "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTczNDkwNTgwLCJleHAiOjE1NzQwOTUzODB9.-qmJLZQmoDuOkhhNN70zfvG3N11Br5Zty1F1REUZMv0" 
   }
 });
 
