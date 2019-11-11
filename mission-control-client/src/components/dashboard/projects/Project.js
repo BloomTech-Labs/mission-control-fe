4cd2932b9f26fe89122e4a28979938b7586a3b9e
Alex <agordon1123@gmail.com>
2019-11-11 17:37:03 -0500

    removed programs from project

--- mission-control-client/src/components/dashboard/projects/Project.js
+++ mission-control-client/src/components/dashboard/projects/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Project = props => {
+  console.log(props);
   const programs = ["web", "ux/ui", "ds"];
 
   return (
@@ -13,32 +14,7 @@ const Project = props => {
       className="project-container"
     >
       <div className="project-description">
-        <h3 className="project-title">{props.el && props.el.project.name}</h3>
-        {new Date(props.el && props.el.project.end) > new Date() ? (
-          <p className="project-status-completed">Completed</p>
-        ) : (
-          <p className="project-status-not-completed">Not Completed</p>
-        )}
-      </div>
-      <div className="project-programs">
-        {programs.map(
-          (el, i) =>
-            (el === "ux/ui" && (
-              <p key={i} className="project-program-avatar program-ux">
-                {el.toUpperCase()}
-              </p>
-            )) ||
-            (el === "ds" && (
-              <p key={i} className="project-program-avatar program-ds">
-                {el.toUpperCase()}
-              </p>
-            )) ||
-            (el === "web" && (
-              <p key={i} className="project-program-avatar program-web">
-                {el.toUpperCase()}
-              </p>
-            ))
-        )}
+        <h3 className="project-title">{props.el.project.name}</h3>
       </div>
     </div>
   );
