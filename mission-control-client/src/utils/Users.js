6d0211b1811e7da72c94a53c04f193e574750afc
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-12-02 12:33:41 -0600

    Test users created for basic app use

---
+++ mission-control-client/src/utils/Users.js
@@ -0,0 +1,26 @@
+
+const users = [
+  {
+    email: 'JohnDoe@gmail.com',
+    password: 'TempPass123!',
+    role: "student",
+    fname: "John",
+    token: "djiafbao;wetfawfawrgvqaweoiugb"
+  },
+  {
+    email: 'TestUser@gmail.com',
+    password: 'TempPass123!',
+    role: "manager",
+    fname: "Test",
+    token: "djiafbao;wetfawfawrgvqaweoiugb"
+  },
+  {
+    email: 'JerrySpringer@gmail.com',
+    password: 'TempPass123!',
+    role: 'admin',
+    fname: 'Jerry',
+    token: "djiafbao;wetfawfawrgvqaweoiugb"
+  }
+]
+
+export default users;
