77e076f5f39f92e630016223e569f8f08db14990
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-12-02 12:35:57 -0600

    changed base URL to point to graphql server

--- mission-control-client/src/utils/axiosLabsGraphQL.js
+++ mission-control-client/src/utils/axiosLabsGraphQL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosLabsGraphQL = axios.create({
-  baseURL: "https://api.use-mission-control.com/",
+  baseURL: "https://api-dev.use-mission-control.com/",
   // headers: {
   //    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTc0MTAwMDIwLCJleHAiOjE1NzQ3MDQ4MjB9.O_-u_vBXfhBfdP21agD9_rb6iz5NSanjbh3HrDuW4GA" 
   // }
