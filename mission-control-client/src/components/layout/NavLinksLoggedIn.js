96e6e97307626f3dd22d175c40d20bec5a8bcbb2
Jason Sonnichsen <Harliquinn@gmail.com>
2019-12-03 11:44:08 -0500

    Ititial setup
    Put Urql in place with basic exchange and a default token to use against the AWS backend. An example query was created as proof of concept on the login page showing a produt list in the console

--- mission-control-client/src/components/layout/NavLinksLoggedIn.js
+++ mission-control-client/src/components/layout/NavLinksLoggedIn.js
@@ -1,14 +1,16 @@
 import React from "react";
-import AvatarMenu from './Avatar';
-import { ReactComponent as Bell } from '../../assets/bell.svg';
+import AvatarMenu from "./Avatar";
+import { ReactComponent as Bell } from "../../assets/bell.svg";
 
 const NavLinksLoggedIn = () => {
-
   const name = localStorage.getItem("fname");
 
   return (
     <>
-      <p data-testid="greeting" className="logged-in logged-in-link">{`Welcome back, ${name ? name : 'User'}`}</p>
+      <p
+        data-testid="greeting"
+        className="logged-in logged-in-link"
+      >{`Welcome back, ${name ? name : "User"}`}</p>
       <AvatarMenu />
     </>
   );
