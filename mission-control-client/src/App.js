3e7c7561bf6e9bf271b83b77107793c650f28eaa
Kyle Guerrero <kyleguerrero85@gmail.com>
2019-12-03 15:34:55 -0600

    OKTA sign in is now in place

--- mission-control-client/src/App.js
+++ mission-control-client/src/App.js
@@ -6,10 +6,11 @@ import {
   Redirect,
   useHistory
 } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
 // components
 import Layout from "./components/layout/Layout";
-import Registration from "./components/auth/Registration.js";
+// import Registration from "./components/auth/Registration.js";
 import Login from "./components/auth/Login";
 import ProjectMore from "./components/dashboard/admin-dashboard/ProjectMore";
 import Bad from "./components/layout/Bad";
@@ -36,32 +37,51 @@ function App() {
     embedAnalytics();
   }, [location]);
 
+  function onAuthRequired({ history }){
+    history.push('/')
+  }
+
   return (
     <Layout>
-      <Switch>
-        {/* {(!localStorage.getItem("role") ||
-          !["admin", "manager", "student"].includes(decrypt())) &&
-          localStorage.removeItem("token") &&
-          history.push("/login")} */}
-        {/* <PrivateRoute path="/" exact>
-          {localStorage.getItem("token") ? (
-            <Redirect to={{ pathname: `${decrypt()}/dashboard` }} />
+      <Security issuer='https://dev-947415.okta.com/oauth2/default'
+                clientId='0oa1sum11uqYEHMzC357'
+                redirectUri={window.location.origin + '/implicit/callback'}
+                onAuthRequired={onAuthRequired}
+                pkce={true} >
+        <Switch>
+          {/* {(!localStorage.getItem("role") ||
+            !["admin", "manager", "student"].includes(decrypt())) &&
+            localStorage.removeItem("token") &&
+            history.push("/login")} */}
+          {/* <PrivateRoute path="/" exact>
+            {localStorage.getItem("token") ? (
+              <Redirect to={{ pathname: `${decrypt()}/dashboard` }} />
+            ) : (
+              <Redirect to="/login" />
+            )}
+          </PrivateRoute> */}
+          {/* OKTA will be taking over user access and control */}
+          {/* <Route path="/register" component={Registration} /> */}
+          {/* <Route path="/" exact component={Login} /> */}
+          <Route path='/login' render={() => <Login baseUrl='https://dev-947415.okta.com' />} />
+          <Route exact={true} path="/">
+            {localStorage.getItem("okta-token-storage") ? (
+            <Redirect to={{ pathname: `/admin/dashboard` }} />
           ) : (
             <Redirect to="/login" />
           )}
-        </PrivateRoute> */}
-        {/* OKTA will be taking over user access and control */}
-        {/* <Route path="/register" component={Registration} /> */}
-        <Route path="/" exact component={Login} />
-        <Route path={`/profile/${localStorage.getItem('fname')}/edit/password`} component={EditProfile} />
-        <Route path={`/profile/${localStorage.getItem('fname')}/edit/email`} component={EditProfile} />
-        <Route path="/student/dashboard" component={UserDash} />
-        <Route exact path="/manager/dashboard" component={AdminDash} />
-        <Route exact path="/admin/dashboard" component={AdminDash} />
-        <Route path={`/admin/${localStorage.getItem('fname')}/edit/promotions`} component={UserPromotions} />
-        <Route path="/admin/dashboard/:id" component={ProjectMore} />
-        <Route component={Bad} />
-      </Switch>
+          </Route>
+          <Route path={`/profile/${localStorage.getItem('fname')}/edit/password`} component={EditProfile} />
+          <Route path={`/profile/${localStorage.getItem('fname')}/edit/email`} component={EditProfile} />
+          <Route path="/student/dashboard" component={UserDash} />
+          <Route exact path="/manager/dashboard" component={AdminDash} />
+          <Route exact path="/admin/dashboard" component={AdminDash} />
+          <Route path={`/admin/${localStorage.getItem('fname')}/edit/promotions`} component={UserPromotions} />
+          <Route path="/admin/dashboard/:id" component={ProjectMore} />
+          <Route path='/implicit/callback' component={ImplicitCallback} />
+          <Route component={Bad} />
+        </Switch>
+      </Security>
     </Layout>
   );
 }
