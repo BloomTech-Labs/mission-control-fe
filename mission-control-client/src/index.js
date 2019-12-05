7f3ecfd460ed908699c88d345a3b52680e2953a0
Steven D. Rollins <22282953+greenhornsr@users.noreply.github.com>
2019-12-04 16:20:58 -0800

    delete cache, env update
    modified cache to account for delete cache, not working yet.  added env variable.

--- mission-control-client/src/index.js
+++ mission-control-client/src/index.js
@@ -37,6 +37,12 @@ const cache = cacheExchange({
             }
           })
       },
+      // deleteProduct: ({ deleteProduct }, _args, cache) => {
+      //   console.log(cache)
+      //   cache.updateQuery({ query: productsU, requestPolicy: 'cache-and-network' }, data => {
+      //     return data
+      //   })
+      // }
     },
   }
 });
@@ -49,8 +55,7 @@ const client = new Client({
     return {
       headers: {
         Authorization:
-        // process.env.REACT_APP_JWT_TOKEN
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkZWZhdWx0QGRlZmF1bHQiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTc1MzA3NDMzLCJleHAiOjE1NzU5MTIyMzN9.Il4acd3O1awN-0yB4pE1S_B4uay3KNCs92oJTNgjNok"
+        process.env.REACT_APP_JWT_TOKEN
       }
     };
   },
