726f95fa123c43d4aae08c5ec6aad963dffa8117
Jason Sonnichsen <Harliquinn@gmail.com>
2019-12-06 12:47:29 -0500

    Fixed Project edit mutation. Removed some control.logs

--- mission-control-client/src/mutations/index.js
+++ mission-control-client/src/mutations/index.js
@@ -9,7 +9,7 @@ export {
   addProject,
   updateProject,
   deleteProject
-}
+};
 
 //Product CRUD
 //Add
@@ -67,7 +67,7 @@ const updateProject = gql`
   ) {
     updateProject(
       where: { id: $projectId }
-      data: { name: $name, product: { connect: { id: $id } } }
+      data: { name: $name, product: { connect: { id: $productId } } }
     ) {
       id
       name
@@ -85,5 +85,3 @@ const deleteProject = gql`
     }
   }
 `;
-
-
