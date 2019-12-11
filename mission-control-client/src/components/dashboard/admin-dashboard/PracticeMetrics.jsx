d55ac444640249abff66918c52e6ac713723e3db
Jason Sonnichsen <Harliquinn@gmail.com>
2019-12-10 19:57:31 -0500

    Fixed bug of project removed from view but not actually deleted

--- mission-control-client/src/components/dashboard/admin-dashboard/PracticeMetrics.jsx
+++ mission-control-client/src/components/dashboard/admin-dashboard/PracticeMetrics.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 export default function PracticeMetrics(props) {
-  console.log("props from practice page", props);
+  // console.log("props from practice page", props);
 
   return <div>{/* hello */}</div>;
 }
