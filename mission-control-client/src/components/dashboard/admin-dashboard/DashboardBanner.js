84efba8a830c70b468968dc1e70d106ab29c3833
Jason Sonnichsen <Harliquinn@gmail.com>
2019-12-17 17:10:09 -0500

    ProjectMore now using our data, but not dynamically. May need code from context branch to make it work. Combining in my feature branch

--- mission-control-client/src/components/dashboard/admin-dashboard/DashboardBanner.js
+++ mission-control-client/src/components/dashboard/admin-dashboard/DashboardBanner.js
@@ -1,23 +1,18 @@
 import React, { useContext } from "react";
 // Context
-import { ProductContext } from "../../../context/ProductContext"
+import { ProductContext } from "../../../context/ProductContext";
 
 const DashboardBanner = () => {
-  const { productState } = useContext(ProductContext)
+  const { productState } = useContext(ProductContext);
   return (
     <div className="dashboard-banner-container">
       <div className="dashboard-banner-head">
-        <p>
-          {productState.active
-            ? productState.active.name
-            : "Loading..."}
-        </p>
+        <p>{productState.active ? productState.active.name : "Loading..."}</p>
       </div>
       <div className="dashboard-product-projects">
         <p className="dashboard-product-total-projects">
           Total Projects:{" "}
-          {productState.active &&
-            productState.active.projects.length}
+          {productState.active && productState.active.projects.length}
         </p>
       </div>
     </div>
