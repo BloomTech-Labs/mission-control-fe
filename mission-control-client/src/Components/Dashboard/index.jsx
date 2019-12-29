2dd1f5bd0f76c2e9223828add2dfa23c9c8f3459
gebhartn <nicholas.gebhart@gmail.com>
2019-12-28 16:31:24 -0800

    chore: dashboard default export
    use index.jsx for root directory export

---
+++ mission-control-client/src/Components/Dashboard/index.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { getData, secureRequest } from '../../Services/tempServices';
+
+const Dashboard = ({ logout, getToken }) => {
+  const [server, setServer] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await secureRequest(getToken, getData);
+      setServer(data);
+    })();
+  }, [getToken]);
+
+  return (
+    <>
+      <pre>{JSON.stringify(server, null, 4)}</pre>
+      <div>This is the Dashboard view</div>
+      <button type="submit" onClick={logout}>
+        Logout
+      </button>
+    </>
+  );
+};
+
+export default Dashboard;
