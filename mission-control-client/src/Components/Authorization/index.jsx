bc5b18cac82ddca0d2240483f5dcbb4b4393c5a1
gebhartn <nicholas.gebhart@gmail.com>
2019-12-31 05:59:43 -0800

    fix: memoize checkAuthentication
    prefer useCallback for authentication check, limit rerenders
    - state was not properly setting, causing infinite renders
    - block level comments for future generations

--- mission-control-client/src/Components/Authorization/index.jsx
+++ mission-control-client/src/Components/Authorization/index.jsx
@@ -1,23 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { withAuth } from '@okta/okta-react';
 
 import Dashboard from '../Dashboard';
+
+// OKTA authentication widget, invokes implicit callback to login
+// if the user attempts to access a protected view.
+// isAuthenticated peers into storage for a valid token upon PKCE
+// handshake, so clear localStorage is called on logout.
 
 export default withAuth(({ auth }) => {
   const [authState, setAuthState] = useState(null);
   const { isAuthenticated, logout, getAccessToken } = auth;
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const authenticated = await isAuthenticated();
-
-      if (authenticated !== authState) {
-        setAuthState({ authenticated });
-      }
-    };
-    checkAuthentication();
+  // Memoized callback for subsequent re-renders of Dashboard children
+  const checkAuthentication = useCallback(async () => {
+    const authenticated = await isAuthenticated();
+    if (authenticated !== authState) {
+      setAuthState(authenticated);
+    }
   }, [authState, isAuthenticated]);
 
+  // useCallback on re-render if dependencies are valid
+  useEffect(() => {
+    checkAuthentication();
+  });
+
+  // Clear localStorage to remove token
   const invokeOktaLogout = async () => {
     localStorage.clear();
     logout('/');
