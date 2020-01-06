7e0cb1576d57644032656533be13f7e39632ce94
gebhartn <nicholas.gebhart@gmail.com>
2020-01-06 07:26:41 -0800

    fix: adds named default exports

--- mission-control-client/src/middleware/Urql.jsx
+++ mission-control-client/src/middleware/Urql.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { cacheExchange as normalizedCache } from '@urql/exchange-graphcache';
 
-const cache = cacheExchange({});
+// The @urql/exchange-graphcache dependency exposes a normalized cache
+// by default, the urql client comes pre-configured with a document cache.
+const cacheExchange = normalizedCache({});
 
 const client = new Client({
   url: `${process.env.REACT_APP_GRAPHQL_URL}`,
-  exchanges: [dedupExchange, cache, fetchExchange],
+  exchanges: [dedupExchange, cacheExchange, fetchExchange],
 });
 
-export default ({ children }) => <Provider value={client}>{children}</Provider>;
+const Urql = ({ children }) => <Provider value={client}>{children}</Provider>;
+
+export default Urql;
