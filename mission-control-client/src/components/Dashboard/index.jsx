5b42b1169bb961aa52c09a9b04f9f91b37d709c4
gebhartn <nicholas.gebhart@gmail.com>
2020-01-06 13:18:19 -0800

    chore: adds dummy project data
    adds project data for styling purposes

--- mission-control-client/src/components/Dashboard/index.jsx
+++ mission-control-client/src/components/Dashboard/index.jsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { getData, secureRequest } from '../../services/tempServices';
+import React from 'react';
 
 const Dashboard = ({ logout, getToken }) => {
-  const [server, setServer] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await secureRequest(getToken, getData);
-      setServer(data);
-    })();
-  }, [getToken]);
-
   return (
     <>
-      <pre>{JSON.stringify(server, null, 4)}</pre>
       <div>This is the Dashboard view</div>
-      <button type="submit" onClick={logout}>
-        Logout
-      </button>
     </>
   );
 };
