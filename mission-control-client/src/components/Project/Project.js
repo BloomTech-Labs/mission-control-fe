99b150b1973a706cd93298f1e92a6e50ed9ed95e
Tony <tonyrkovar@gmail.com>
2020-01-06 19:14:40 -0600

    CHORE added classnames and styling file

--- mission-control-client/src/components/Project/Project.js
+++ mission-control-client/src/components/Project/Project.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import NotesFeed from './NotesFeed';
+import Sidebar from '../Layout/Sidebar';
+
+import styles from '../../styles/notes.module.scss'
 
 export default () => {
   return (
-    <>
+    <div className={styles["project-container"]}>
+      <Sidebar />
       <NotesFeed />
-    </>
+    </div>
   );
 };
