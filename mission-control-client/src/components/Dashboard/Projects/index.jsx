c75ef2a7200b6adb1d25084f86293c0f39939808
gebhartn <nicholas.gebhart@gmail.com>
2020-01-07 15:30:52 -0800

    feat: adds naive useQuery to projects
    bringing in data from first implementation of urql authentication

--- mission-control-client/src/components/Dashboard/Projects/index.jsx
+++ mission-control-client/src/components/Dashboard/Projects/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useQuery } from 'urql';
 import gql from 'graphql-tag';
+import { Link } from 'react-router-dom';
+
+import { getToken } from '../../../services/tempServices';
 
 import styles from '../../../styles/projects.module.scss';
 
@@ -13,33 +16,38 @@ const FEED_QUERY = gql`
       status
       update
     }
-  }Expected Iterable, but did not find one for field Query.info.
+  }
 `;
 
 const Projects = () => {
   const [state] = useQuery({ query: FEED_QUERY });
-  console.log(state);
+  const { data, error, fetching } = state;
+
+  if (fetching) return <div>fetching</div>;
 
   return (
-    <pre>{JSON.stringify(state, null, 4)}</pre>
-    // {/* <table> */}
-    // {/*   <tr> */}
-    // {/*     <th>Project Name</th> */}
-    // {/*     <th>Section Lead</th> */}
-    // {/*     <th>Team Lead</th> */}
-    // {/*     <th className={styles.rtd}>Last Updated</th> */}
-    // {/*     <th className={styles.rtc}>Status</th> */}
-    // {/*   </tr> */}
-    // {/*   {dummyData.map(project => ( */}
-    // {/*     <tr> */}
-    // {/*       <td className={styles.title}>{project.project}</td> */}
-    // {/*       <td>{project.section_lead}</td> */}
-    // {/*       <td>{project.team_lead}</td> */}
-    // {/*       <td className={styles.rtd}>{project.update}</td> */}
-    // {/*       <td className={styles.rtc}>{project.status}</td> */}
-    // {/*     </tr> */}
-    // {/*   ))} */}
-    // {/* </table> */}
+    <table>
+      <tr>
+        <th>Project Name</th>
+        <th>Section Lead</th>
+        <th>Team Lead</th>
+        <th className={styles.rtd}>Last Updated</th>
+        <th className={styles.rtc}>Status</th>
+      </tr>
+      {data.info.map(project => (
+        <tr>
+          <td className={styles.title}>
+            <Link to="/" className={styles.title}>
+              {project.project}
+            </Link>
+          </td>
+          <td>{project.section_lead}</td>
+          <td>{project.team_lead}</td>
+          <td className={styles.rtd}>{project.update}</td>
+          <td className={styles.rtc}>{project.status}</td>
+        </tr>
+      ))}
+    </table>
   );
 };
 
