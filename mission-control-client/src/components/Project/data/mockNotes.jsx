c2543c4ab29e8b00e8f773b49c51309c855e2c5f
Tony <tonyrkovar@gmail.com>
2020-01-07 12:09:03 -0600

    FEAT Drop down avatars flexwrapped

--- mission-control-client/src/components/Project/data/mockNotes.jsx
+++ mission-control-client/src/components/Project/data/mockNotes.jsx
@@ -28,6 +28,40 @@ export default [
         avatar:
           'https://ca.slack-edge.com/T4JUEB3ME-UE60KE0CU-a447a0d2fb96-512',
       },
+      {
+        firstName: 'Ryan',
+        lastName: 'Holdaway',
+        avatar:
+          'https://ca.slack-edge.com/T4JUEB3ME-U9E7020TX-4e37d09c9c61-512',
+      },
+      {
+        firstName: 'Parth',
+        lastName: 'Shah',
+        avatar: 'https://ca.slack-edge.com/T4JUEB3ME-UQ8RC9QG3-5a7f8584c858-72',
+      },
+      {
+        firstName: 'Ronny',
+        lastName: 'Alvarado',
+        avatar:
+          'https://ca.slack-edge.com/T4JUEB3ME-UE60KE0CU-a447a0d2fb96-512',
+      },
+      {
+        firstName: 'Ryan',
+        lastName: 'Holdaway',
+        avatar:
+          'https://ca.slack-edge.com/T4JUEB3ME-U9E7020TX-4e37d09c9c61-512',
+      },
+      {
+        firstName: 'Parth',
+        lastName: 'Shah',
+        avatar: 'https://ca.slack-edge.com/T4JUEB3ME-UQ8RC9QG3-5a7f8584c858-72',
+      },
+      {
+        firstName: 'Ronny',
+        lastName: 'Alvarado',
+        avatar:
+          'https://ca.slack-edge.com/T4JUEB3ME-UE60KE0CU-a447a0d2fb96-512',
+      },
     ],
   },
   {
