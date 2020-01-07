bf81694370c45cd84c627511772b54ff2b251dc1
Tony <tonyrkovar@gmail.com>
2020-01-06 19:14:40 -0600

    CHORE added classnames and styling file

--- mission-control-client/src/components/Project/NotesFeed.js
+++ mission-control-client/src/components/Project/NotesFeed.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import Note from './Note';
 import mockNotes from './data/mockNotes';
 
+import styles from '../../styles/noteFeed.module.scss';
+
 export default () => {
   const [notes, setNotes] = useState(mockNotes);
   return (
-    <div>
+    <div className={styles['note-container']}>
       {notes.map(note => {
         return <Note note={note} />;
       })}
