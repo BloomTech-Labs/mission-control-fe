9912daba8829f1add3d8a523c8c5c1ce6a8f9858
Tony <tonyrkovar@gmail.com>
2020-01-06 18:29:34 -0600

    CHORE Adjusted classnames for scss

--- mission-control-client/src/components/Project/Note.js
+++ mission-control-client/src/components/Project/Note.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Rating from '@material-ui/lab/Rating';
 
+import styles from '../../styles/notes.module.scss';
+
 export default ({
   note: { author, title, body, rating, attachment, attendees, tags },
 }) => {
@@ -9,23 +11,23 @@ export default ({
   console.log(attendees);
   return (
     <section>
-      <div className="avatar-container">
-        <img src={avatar} alt={firstName} className="avatar" />
+      <div className={styles['avatar-container']}>
+        <img src={avatar} alt={firstName} className={styles['avatar']} />
       </div>
-      <div className="note-container">
-        <div className="note-header">
+      <div className={styles['note-container']}>
+        <div className={styles['note-header']}>
           {title}
           <Rating name="rating" value={rating} readOnly />
         </div>
-        <div className="note-body">{body}</div>
-        <div className="note-footer">
+        <div className={styles['note-body']}>{body}</div>
+        <div className={styles['note-footer']}>
           <div
-            className={expanded ? 'expanded' : 'linear'}
+            className={expanded ? styles['expanded'] : styles['linear']}
             onClick={() => setExpanded(!expanded)}
           >
             {attendees.map(attendee => {
               return (
-                <div className="mini-avatar-container">
+                <div className={styles['mini-avatar-container']}>
                   <img src={attendee.avatar} />
                   <p>
                     {attendee.firstName} {attendee.lastName}
@@ -34,7 +36,7 @@ export default ({
               );
             })}
           </div>
-          <button className="edit-note-btn">Edit</button>
+          <button className={styles['edit-note-btn']}>Edit</button>
         </div>
       </div>
     </section>
