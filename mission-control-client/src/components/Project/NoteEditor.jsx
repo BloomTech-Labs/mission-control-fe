b40476f3c0f7449f3d1f40251a1bfcc74917e675
Vyraal1 <kevin.afable@gmail.com>
2020-01-08 14:05:45 -0500

    FEAT attendee deletion

--- mission-control-client/src/components/Project/NoteEditor.jsx
+++ mission-control-client/src/components/Project/NoteEditor.jsx
@@ -17,61 +17,76 @@ export default ({ user, team }) => {
   const [expanded, setExpanded] = useState(false);
   const [attendees, setAttendees] = useState(team);
 
+  const deleteAttendee = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const deleted = e.target.previousSibling.textContent.split(' ');
+    const newAttendees = attendees.filter(({ firstName, lastName }) => {
+      return firstName != deleted[0] && lastName != deleted[1];
+    });
+    setAttendees(newAttendees);
+  };
+
   return (
-    <div className={styles['editor-container']}>
-      <div className={styles['avatar-container']}>
-        <img src={user.avatar} />
-      </div>
-      <form>
-        <div className={styles['form-header']}>
-          <input
-            placeholder="Meeting Topic"
-            type="text"
-            name="title"
-            onChange={e => setTitle(e.target.value)}
-          />
-          <StarRatings
-            numberOfStars={3}
-            name="rating"
-            starRatedColors="rgb(245,73,135)"
-            starHoverColor="rgb(245,73,135)"
-            starEmptyColor="rgba(245,73,135,.2)"
-            changeRating={rating => setRating(rating)}
-            starDimension="20px"
-            starSpacing=".5px"
-          />
+    <div>
+      <h2>Project Notes</h2>
+      <div className={styles['editor-container']}>
+        <div className={styles['avatar-container']}>
+          <img src={user.avatar} />
         </div>
-        <input
-          className={styles['body-input']}
-          placeholder="What's going on?"
-          name="body"
-          onChange={e => setBody(e.target.value)}
-        />
-        <div className={styles['text-footer']}>
-          <div
-            className={expanded ? styles['expanded'] : styles['collapsed']}
-            onClick={() => setExpanded(!expanded)}
-          >
-            <div className={styles['attendees-avatars']}>
-              {attendees.map(attendee => {
-                return (
-                  <div className={styles['mini-avatar-container']}>
-                    <img src={attendee.avatar} />
-                    <p>
-                      {attendee.firstName} {attendee.lastName}
-                    </p>
-                    <button>x</button>
-                  </div>
-                );
-              })}
+        <form className={styles['form-container']}>
+          <div className={styles['form-header']}>
+            <input
+              placeholder="Meeting Topic"
+              type="text"
+              name="title"
+              onChange={e => setTitle(e.target.value)}
+            />
+            <StarRatings
+              numberOfStars={3}
+              name="rating"
+              starRatedColors="rgb(245,73,135)"
+              starHoverColor="rgb(245,73,135)"
+              starEmptyColor="rgba(245,73,135,.2)"
+              changeRating={rating => setRating(rating)}
+              starDimension="20px"
+              starSpacing=".5px"
+            />
+          </div>
+          <div className={styles['body-container']}>
+            <textarea
+              className={styles['body-input']}
+              placeholder="What's going on?"
+              name="body"
+              onChange={e => setBody(e.target.value)}
+            />
+          </div>
+          <div className={styles['text-footer']}>
+            <div
+              className={expanded ? styles['expanded'] : styles['collapsed']}
+              onClick={() => setExpanded(!expanded)}
+            >
+              <div className={styles['attendees-avatars']}>
+                {attendees.map(attendee => {
+                  return (
+                    <div className={styles['mini-avatar-container']}>
+                      <img src={attendee.avatar} />
+                      <p>
+                        {attendee.firstName} {attendee.lastName}
+                      </p>
+                      <button onClick={deleteAttendee}>x</button>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className="button-container">
+              <button>Attach</button>
+              <button>Save</button>
             </div>
           </div>
-          <div className="button-container">
-            <button>Attach</button>
-            <button>Save</button>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 };
