c9bc8b19016b56437106998c42e5811c3a579fb3
Vyraal1 <kevin.afable@gmail.com>
2020-01-07 19:08:03 -0500

    CHORE renaming extensions

--- mission-control-client/src/components/Project/Project.jsx
+++ mission-control-client/src/components/Project/Project.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+
 import NotesFeed from './NotesFeed';
 import Sidebar from '../Layout/Sidebar';
 import Team from './Team';
+import NoteEditor from './NoteEditor';
+
 import styles from '../../styles/notes.module.scss';
 
 import teamList from './data/mockTeam';
+import mockSession from './data/mockEditor';
 
 export default () => {
   return (
     <div className={styles['project-container']}>
       <Sidebar />
       {/* Header */}
-      {/* Input Field */}
+      <NoteEditor user={mockSession.user} team={mockSession.attendees} />
       <NotesFeed />
       {/* Team */}
       <Team team={teamList} />
