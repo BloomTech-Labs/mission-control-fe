de86fcd0bc7a03bb191fc3bc62386bd45e1b5272
gebhartn <nicholas.gebhart@gmail.com>
2020-01-07 18:47:47 -0800

    chore: adds utils directory

---
+++ mission-control-client/src/utils/getToken.js
@@ -0,0 +1,8 @@
+export default () => {
+  const token = localStorage.getItem('okta-token-storage');
+  const {
+    accessToken: { accessToken },
+  } = JSON.parse(token);
+
+  return accessToken;
+};
