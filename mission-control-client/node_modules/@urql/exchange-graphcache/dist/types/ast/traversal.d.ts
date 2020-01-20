9f41d6a536433519d4dda0aeb2fc964ec399b1b7
gebhartn <nicholas.gebhart@gmail.com>
2020-01-19 16:35:38 -0800

    fix: node modules

--- mission-control-client/node_modules/@urql/exchange-graphcache/dist/types/ast/traversal.d.ts
+++
@@ -1,7 +0,0 @@
-import { SelectionNode, DocumentNode, OperationDefinitionNode } from 'graphql';
-import { Fragments, Variables } from '../types';
-/** Returns the main operation's definition */
-export declare const getMainOperation: (doc: DocumentNode) => OperationDefinitionNode;
-/** Returns a mapping from fragment names to their selections */
-export declare const getFragments: (doc: DocumentNode) => Fragments;
-export declare const shouldInclude: (node: SelectionNode, vars: Variables) => boolean;
--- mission-control-client/node_modules/@urql/exchange-graphcache/dist/types/extras/relayPagination.d.ts
+++
@@ -1,6 +0,0 @@
-import { Resolver } from '../types';
-export declare type MergeMode = 'outwards' | 'inwards';
-export interface PaginationParams {
-    mergeMode?: MergeMode;
-}
-export declare const relayPagination: (params?: PaginationParams) => Resolver;
