700ea5523441c1dae652362bbaf5c5985cbed4a0
gebhartn <nicholas.gebhart@gmail.com>
2020-01-20 20:13:19 -0800

    feat: role-based data

--- src/components/ProjectListView/index.jsx
+++ src/components/ProjectListView/index.jsx
@@ -8,23 +8,16 @@ import styles from '../../styles/projects.module.scss';
 
 const PROJECT_LIST_VIEW = gql`
   query {
-    products {
-      id
-      name
+    me {
       projects {
-        id
         name
         status
         updatedAt
         sectionLead {
-          id
           name
-          role
         }
         teamLead {
-          id
           name
-          role
         }
       }
     }
@@ -33,50 +26,41 @@ const PROJECT_LIST_VIEW = gql`
 
 const ProjectListView = ({ logout }) => {
   const [state] = useQuery({ query: PROJECT_LIST_VIEW });
-  const { data, fetching } = state;
+  const { data } = state;
 
-  return (
-    <div>
-      <button type="submit" onClick={logout}>
-        Clicking Space
-      </button>
-      {!fetching ? (
-        <table>
-          <thead>
+  if (data && data.me.projects.length) {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Project Name</th>
+            <th>Section Lead</th>
+            <th>Team Lead</th>
+            <th className={styles.rtd}>Last Updated</th>
+            <th className={styles.rtc}>Status</th>
+          </tr>
+        </thead>
+        {data.me.projects.map(project => (
+          <tbody>
             <tr>
-              <th>Project Name</th>
-              <th>Section Lead</th>
-              <th>Team Lead</th>
-              <th className={styles.rtd}>Last Updated</th>
-              <th className={styles.rtc}>Status</th>
+              <td key={project.id} className={styles.title}>
+                <Link to="/" className={styles.title}>
+                  {project.name}
+                </Link>
+              </td>
+              <td>{project.sectionLead.name}</td>
+              <td>{project.teamLead.name}</td>
+              <td className={styles.rtd}>{mapTime(project.updatedAt)} ago</td>
+              {/* <td className={styles.rtd}>Yesterday</td> */}
+              <td className={styles.rtc}>{project.status ? 'OK' : 'Fire'}</td>
             </tr>
-          </thead>
-          {data.products.map(product => (
-            <tbody>
-              <tr>
-                <td key={product.id} className={styles.title}>
-                  <Link to="/" className={styles.title}>
-                    {product.name}
-                  </Link>
-                </td>
-                <td>{product.projects[0].sectionLead.name}</td>
-                <td>{product.projects[0].teamLead.name}</td>
-                <td className={styles.rtd}>
-                  {`${mapTime(product.projects[0].updatedAt)}  ago`}
-                </td>
-                {/* <td className={styles.rtd}>Yesterday</td> */}
-                <td className={styles.rtc}>
-                  {product.projects[0].status ? 'OK' : 'Fire'}
-                </td>
-              </tr>
-            </tbody>
-          ))}
-        </table>
-      ) : (
-        <div>fetching</div>
-      )}
-    </div>
-  );
+          </tbody>
+        ))}
+      </table>
+    );
+  } else {
+    return <h1>loading</h1>;
+  }
 };
 
 export default ProjectListView;
