b0d0e4a7071ef6ae12145110961921820c6f92da
unknown <iggylow1@gmail.com>
2020-01-28 12:45:35 -0500

    chore: renaming, linting, aliasing
    * query.js -> requests.js, will house mutations and queries for Project view
    * linting

--- src/components/Project/Header.jsx
+++ src/components/Project/Header.jsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 import styles from '../../styles/projectHeader.module.scss';
 import Fire from '../../images/fire.png';
 
-export default ( {project, product, status} ) => {
-
+export default ({ project, product, status }) => {
   // Sanitize string inputs to remove Product prefix
   const cleanName = str => {
     const match = str.match(/Labs \d{1,3} -(.+)/);
@@ -29,16 +28,18 @@ export default ( {project, product, status} ) => {
             <p className={styles['project-cohort']}> {product.name} </p>
           </div>
           <div className={styles['project-status-container']}>
-
-            { project.status ? (
+            {project.status ? (
               ''
             ) : (
               <>
-                <img src={Fire} alt="project-fire" className={styles['project-fire']} />
+                <img
+                  src={Fire}
+                  alt="project-fire"
+                  className={styles['project-fire']}
+                />
                 <p className={styles['project-status']}> Falling behind! </p>
               </>
             )}
-            
           </div>
         </div>
       </div>
