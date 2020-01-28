e7bd0bd94e91d51cb1473fc9483ff3b4a4032855
Tony <tonyrkovar@gmail.com>
2020-01-27 19:34:29 -0600

    feat: pulled data from the parent component to render notes for a project

--- src/components/Project/NotesFeed.jsx
+++ src/components/Project/NotesFeed.jsx
@@ -4,13 +4,20 @@ import mockNotes from './data/mockNotes';
 
 import styles from '../../styles/noteFeed.module.scss';
 
-export default () => {
-  const [notes, setNotes] = useState(mockNotes);
-  return (
-    <div className={styles['notes-container']}>
-      {notes.map(note => {
-        return <Note note={note} />;
-      })}
-    </div>
-  );
+export default ({ notes }) => {
+  // const [notes, setNotes] = useState(mockNotes);
+  console.log(notes);
+  if (notes && notes.length) {
+    console.log(notes);
+    return (
+      <div className={styles['notes-container']}>
+        {notes.map(note => {
+          return <Note note={note} />;
+        })}
+      </div>
+    );
+  } else if (notes.length === 0) {
+    return <h2>There are no notes.</h2>;
+  }
+  return <h2>Loading...</h2>;
 };
