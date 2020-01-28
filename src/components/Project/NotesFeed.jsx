71e2bce9716e7f054666ff84d64303cd595f507c
Roy Wakumelo <roywakumelojr@gmail.com>
2020-01-27 23:16:07 -0500

    Chore: Cleaned up project warnings

--- src/components/Project/NotesFeed.jsx
+++ src/components/Project/NotesFeed.jsx
@@ -5,7 +5,7 @@ import mockNotes from './data/mockNotes';
 import styles from '../../styles/noteFeed.module.scss';
 
 export default () => {
-  const [notes, setNotes] = useState(mockNotes);
+  const [notes] = useState(mockNotes);
   return (
     <div className={styles['notes-container']}>
       {notes.map(note => {
