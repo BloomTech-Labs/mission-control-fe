538d68fc29cbf5ca9cab77b99dc50598e8fade57
Tony Kovar <tonyrkovar@gmail.com>
2020-01-28 15:29:54 -0600

    chore: Changing work stations

--- src/components/Project/NotesFeed.jsx
+++ src/components/Project/NotesFeed.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
 import Note from './Note';
+import NoteFeedEdit from './NoteFeedEdit'
 import mockNotes from './data/mockNotes';
 
 import styles from '../../styles/noteFeed.module.scss';
 
 export default ({ notes }) => {
   // const [notes, setNotes] = useState(mockNotes);
+  const [isEditing, setIsEditing] = useState(false)
   console.log(notes);
   if (notes && notes.length) {
     console.log(notes);
     return (
       <div className={styles['notes-container']}>
-        {notes.map(note => {
-          return <Note note={note} />;
+        {isEditing === false ? 
+        notes.map(note => {
+          
+          return (
+            <> 
+              <Note note={note} />
+              <button onClick={() => {
+                setIsEditing(!isEditing)
+                }}>edit</button>
+            </>
+          );
+        })
+        :
+        notes.map(note => {
+          return <NoteFeedEdit note={note}/>
         })}
       </div>
     );
