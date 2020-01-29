6dd6be9d6036eea519a7f78a2f3de792aafd6022
Roy Wakumelo <roywakumelojr@gmail.com>
2020-01-29 18:13:31 -0500

    Chore: readded the missing dropdown import

--- src/components/Project/NoteEditor.jsx
+++ src/components/Project/NoteEditor.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import StarRatings from 'react-star-ratings';
-import { Label } from 'semantic-ui-react';
+import { Label, Dropdown } from 'semantic-ui-react';
 
 import { useMutation } from 'urql';
 import extractAvatar from '../../utils/managers';
