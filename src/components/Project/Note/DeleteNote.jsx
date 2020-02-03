87e541d795189ac43ca68bc88b0107eb4d3f9bc5
Roy Wakumelo <roywakumelojr@gmail.com>
2020-02-03 11:38:11 -0500

    Chore: added a delete component

---
+++ src/components/Project/Note/DeleteNote.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useMutation } from 'urql';
+
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import { DeleteNoteMutation as deleteNote } from '../Queries/requests';
+
+import { deleteButton } from './Notes.module.scss';
+
+export default function DeleteNote({ id }) {
+  console.log(id);
+
+  const [open, setOpen] = React.useState(false);
+
+  const [, executeMutation] = useMutation(deleteNote);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={deleteButton}>
+      <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+        Delete
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">Are you sure?</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            you would like to delete this note
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="default">
+            Nevermind
+          </Button>
+          <Button
+            color="secondary"
+            autoFocus
+            // onClick={deleteNote({ variables: { id } })}
+          >
+            Let's do it
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
