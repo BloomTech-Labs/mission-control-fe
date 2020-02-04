3a9d72878fa0088b80f1b5441e23ebd52a195c70
Vyraal1 <kevin.afable@gmail.com>
2020-02-04 11:38:57 -0500

    fix: moving managers to members

--- src/utils/managers.js
+++ src/utils/managers.js
@@ -72,6 +72,36 @@ const managers = [
     email: 'julia.richert@lambdaschool.com',
     avatar: 'https://ca.slack-edge.com/T4JUEB3ME-UQM09F9H6-0a01e95783be-512',
   },
+  {
+    name: 'Kevin Afable',
+    email: 'kevin.afable@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-ULJ1MK9GT-17419b760e18-512',
+  },
+  {
+    name: 'Nicholas Gebhart',
+    email: 'nicholas.gebhart@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-ULXHMK9PY-013dd2da8dd8-512',
+  },
+  {
+    name: 'Dakotah Huey',
+    email: 'hueydakotah@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-ULP6AEAF3-7aa2e18de5a8-512',
+  },
+  {
+    name: 'Tony Kovar',
+    email: 'tonykovar@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-ULXJ07DJS-d95403332534-24',
+  },
+  {
+    name: 'Tommy Coleman',
+    email: 'tommycoleman87@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-UHXNFRBFE-b1accb251340-512',
+  },
+  {
+    name: 'Roy Wakumelo Jr',
+    email: 'roywakumelojr@gmail.com',
+    avatar: 'https://ca.slack-edge.com/T4JUEB3ME-ULXALGWPR-90c177b51aa7-512',
+  },
 ];
 
 export default email => {
