35829428d3b84ae41466df828382da6a1e6da4ad
gebhartn <nicholas.gebhart@gmail.com>
2020-02-04 18:19:57 -0800

    chore: renames cache

--- src/middleware/Urql.jsx
+++ src/middleware/Urql.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
-import { cacheExchange as normalizedCache } from '@urql/exchange-graphcache';
+import { cacheExchange } from '@urql/exchange-graphcache';
 import { getToken } from '../utils';
 
 // The @urql/exchange-graphcache dependency exposes a normalized cache
 // by default, the urql client comes pre-configured with a document cache.
-const cacheExchange = normalizedCache({});
+const cache = cacheExchange({});
 
 const client = new Client({
   url: `${process.env.REACT_APP_URQL_URL}`,
-  exchanges: [dedupExchange, cacheExchange, fetchExchange],
+  exchanges: [dedupExchange, cache, fetchExchange],
   fetchOptions: () => {
     const token = getToken();
     return {
