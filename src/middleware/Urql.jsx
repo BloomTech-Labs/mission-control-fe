f2f45e37b3951ea54390c96c06053ab846e66e3c
Dakotah Huey <hueydakotah@gmail.com>
2020-02-06 14:16:14 -0500

    Revert "Feat subscription"

--- src/middleware/Urql.jsx
+++ src/middleware/Urql.jsx
@@ -1,36 +1,14 @@
 import React from 'react';
-import {
-  Provider,
-  Client,
-  dedupExchange,
-  fetchExchange,
-  subscriptionExchange,
-} from 'urql';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
 import { cacheExchange as normalizedCache } from '@urql/exchange-graphcache';
 import { getToken } from '../utils';
 
-import { NOTE_FEED_QUERY } from '../components/Project/Queries';
+import { NOTE_FEED_QUERY, DELETE_NOTE } from '../components/Project/Queries';
 
 // The @urql/exchange-graphcache dependency exposes a normalized cache
 // by default, the urql client comes pre-configured with a document cache.
 const cacheExchange = normalizedCache({
   updates: {
-    Subscription: {
-      newNote: ({ newNote }, args, cache) => {
-        const {
-          project: { id },
-        } = newNote;
-        const variables = { id };
-        cache.updateQuery({ query: NOTE_FEED_QUERY, variables }, data => {
-          if (data !== null) {
-            data.project.notes.unshift(newNote);
-            return data;
-          }
-          return null;
-        });
-      },
-    },
     Mutation: {
       createNote: ({ createNote }, { id }, cache) => {
         const variables = { id };
@@ -46,32 +24,9 @@ const cacheExchange = normalizedCache({
   },
 });
 
-// Subscription client is passed into the client instance
-const subscriptionClient = new SubscriptionClient(
-  `${process.env.REACT_APP_URQL_WS}`,
-  {
-    reconnect: true,
-    connectionParams: {
-      authToken: () => {
-        const token = getToken();
-        return {
-          token,
-        };
-      },
-    },
-  }
-);
-
 const client = new Client({
   url: `${process.env.REACT_APP_URQL_URL}`,
-  exchanges: [
-    dedupExchange,
-    cacheExchange,
-    fetchExchange,
-    subscriptionExchange({
-      forwardSubscription: operation => subscriptionClient.request(operation),
-    }),
-  ],
+  exchanges: [dedupExchange, cacheExchange, fetchExchange],
   fetchOptions: () => {
     const token = getToken();
     return {
