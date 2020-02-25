f9125fa90cd25f103ed6e06f42f26e689cf28e03
Ethan Charles Hoover <echoover@Joshuas-MacBook-Pro.local>
2020-02-25 09:20:02 -0800

    removed status from Queries index.js

--- src/components/Project/Queries/index.js
+++ src/components/Project/Queries/index.js
@@ -91,7 +91,6 @@ export const PROJECT_VIEW_QUERY = gql`
     project(id: $id) {
       id
       name
-      status
       product {
         id
         name
@@ -196,3 +195,10 @@ export const DELETE_NOTE = gql`
     }
   }
 `;
+
+// export const CREATE_LABEL = gql`
+//  mutation createLabel(
+//    $name: String!
+//    $color: String!
+//  )
+// `
