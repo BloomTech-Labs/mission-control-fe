fa4e73fe0a1d67ad41bcf1efe207c3f193bc9ed5
Robbie Porter <rthomporter@gmail.com>
2020-02-26 14:38:10 -0600

    Moved Settings save button/ cleaned up console

--- src/components/Settings/CreateLabel.jsx
+++ src/components/Settings/CreateLabel.jsx
@@ -1,30 +1,14 @@
-import React, {useState, useContext, useCallback} from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
-import {Button, UncontrolledPopover, PopoverBody} from 'reactstrap';
-import {useMutation} from 'urql';
-import {CirclePicker} from 'react-color';
-import {CREATE_LABEL as createLabel} from '../Project/Queries';
+import { Button, UncontrolledPopover, PopoverBody } from 'reactstrap';
+import { CirclePicker } from 'react-color';
 
+import { LabelContext } from '../../contexts/LabelContext';
 
-import {LabelContext} from '../../contexts/LabelContext';
+const CreateLabelForm = () => {
+  const { label, setLabel } = useContext(LabelContext);
 
-
-const CreateLabelForm = (props) => {
-    const {label, setLabel} = useContext(LabelContext);
-
-    const [, executeCreate] = useMutation(createLabel)
-
-    const handleSubmit = useCallback(
-        e => {
-            e.preventDefault()
-            executeCreate(label)
-            props.toggle()
-            setLabel({id:'', name: '', color: ''})
-        },
-        [executeCreate, label]
-    );
-
-    const LabelPreviewColor = styled.div`
+  const LabelPreviewColor = styled.div`
     color: white;
     text-align: center;
     padding-top: 2px;
@@ -36,78 +20,74 @@ const CreateLabelForm = (props) => {
     background: ${label.color};
   `;
 
-    const handleChanges = e => {
-        e.preventDefault();
-        setLabel({
-            ...label,
-            [e.target.name]: e.target.value
-        });
-    };
+  const handleChanges = e => {
+    e.preventDefault();
+    setLabel({
+      ...label,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleColorChanges = color => {
-        setLabel({
-            ...label,
-            color: color.hex
-        });
-    };
+  const handleColorChanges = color => {
+    setLabel({
+      ...label,
+      color: color.hex,
+    });
+  };
 
-    return (
-        <form>
+  return (
+    <form>
+      <div>
+        <h4>Create New Label</h4>
+        <div>
+          <label>
+            Label Name:
+            <input
+              name="name"
+              id="name"
+              placeholder="label..."
+              onChange={handleChanges}
+              value={label.name}
+            />
+          </label>
+          <br />
+          {label.name && label.color ? (
+            <LabelPreviewColor>{label.name}</LabelPreviewColor>
+          ) : (
+            ''
+          )}
+          <label>
             <div>
-                <h4>Create New Label</h4>
-                <div>
-                    <label>
-                        Label Name:
-                        <input
-                            name='name'
-                            id='name'
-                            placeholder='label...'
-                            onChange={handleChanges}
-                            value={label.name}
-                        />
-                    </label>
-                    <br />
-                    {label.name && label.color ? (
-                        <LabelPreviewColor>{label.name}</LabelPreviewColor>
-                    ) : (
-                        ''
-                    )}
-                    <label>
-                        <div>
-                            <Button id='PopoverLegacy' type='button'>
-                                Choose Color
-                            </Button>
-                            <UncontrolledPopover
-                            trigger="legacy"
-                            placement="bottom"
-                            target="PopoverLegacy"
-                            >
-                            <PopoverBody>
-                                <CirclePicker
-                                color={label.color}
-                                colors={[
-                                    '#75a9b6',
-                                    '#575a7b',
-                                    '#27213d',
-                                    '#2c6049',
-                                    '#d19c18',
-                                    '#d42c08',
-                                ]}
-                                onChange={handleColorChanges}
-                                width="130px"
-                                />
-                            </PopoverBody>
-                            </UncontrolledPopover>
-                        </div>
-                    </label>
-                    <Button color='primary' onClick={handleSubmit}>
-                        Save
-                    </Button>
-                </div>
+              <Button id="PopoverLegacy" type="button">
+                Choose Color
+              </Button>
+              <UncontrolledPopover
+                trigger="legacy"
+                placement="bottom"
+                target="PopoverLegacy"
+              >
+                <PopoverBody>
+                  <CirclePicker
+                    color={label.color}
+                    colors={[
+                      '#75a9b6',
+                      '#575a7b',
+                      '#27213d',
+                      '#2c6049',
+                      '#d19c18',
+                      '#d42c08',
+                    ]}
+                    onChange={handleColorChanges}
+                    width="130px"
+                  />
+                </PopoverBody>
+              </UncontrolledPopover>
             </div>
-        </form>
-    )
-
-}
+          </label>
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default CreateLabelForm;
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,26 +1,16 @@
 /* eslint react/no-multi-comp: 0, react/prop-types: 0 */
 import React, { useState, useCallback, useContext } from 'react';
-import styled from 'styled-components';
 import { bottomLinks } from './Settings.module.scss';
-import {
-  Button,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  UncontrolledPopover,
-  PopoverBody,
-} from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { useMutation } from 'urql';
-import { CirclePicker } from 'react-color';
 import { CREATE_LABEL as createLabel } from '../Project/Queries';
 
 import LabelList from './LabelList';
 import CreateLabelForm from './CreateLabel';
-import {LabelContext} from '../../contexts/LabelContext'
+import { LabelContext } from '../../contexts/LabelContext';
 
 const Settings = props => {
-  const {label, setLabel} = useContext(LabelContext)
+  const { label, setLabel } = useContext(LabelContext);
 
   const { className } = props;
 
@@ -30,59 +20,17 @@ const Settings = props => {
 
   const toggle = () => setModal(!modal);
 
-  const initState = {
-    name: '',
-    color: '',
-  };
-
-  const [form, setForm] = useState(initState);
-
   const handleSubmit = useCallback(
     e => {
       e.preventDefault();
-      executeCreate(form);
-      // console.log('Handle submit', handleSubmit);
+      executeCreate(label);
       toggle();
-      setForm(initState);
+      setLabel({ id: '', name: '', color: '' });
     },
-    [executeCreate, form, initState, toggle]
+    [executeCreate, label, setLabel]
   );
 
-  const LabelPreviewColor = styled.div`
-    color: white;
-    text-align: center;
-    padding-top: 2px;
-    margin-bottom: 10px;
-    width: 60px;
-    height: 20px;
-    border-radius: 25px;
-    font-size: 0.8rem;
-    background: ${form.color};
-  `;
-
-  // const LabelDiv = styled.div`
-  //   display: flex;
-  //   justify-content: space-around;
-  // `;
-
-  // console.log('form', form);
-
-  const handleChanges = e => {
-    e.preventDefault();
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleColorChanges = color => {
-    setForm({
-      ...form,
-      color: color.hex,
-    });
-  };
-
-  console.log('label global state', label)
+  // console.log('label global state', label);
 
   return (
     <div className={bottomLinks}>
@@ -93,11 +41,14 @@ const Settings = props => {
         <ModalHeader toggle={toggle}>Settings</ModalHeader>
         <ModalBody>
           {/* <LabelDiv> */}
-          <CreateLabelForm toggle={toggle}/>
+          <CreateLabelForm toggle={toggle} />
           <LabelList />
           {/* </LabelDiv> */}
         </ModalBody>
         <ModalFooter>
+          <Button color="primary" onClick={handleSubmit}>
+            Save
+          </Button>
           <Button color="secondary" onClick={toggle}>
             Cancel
           </Button>
