a3d4906177d793df8185719f2591a3c6e5830fed
unknown <17adamsbryk@washk12.org>
2020-02-27 15:37:49 -0700

    Refactored lettergrade to include lettergrade container

--- src/components/ProjectList/CodeClimate/LetterGrade.jsx
+++ src/components/ProjectList/CodeClimate/LetterGrade.jsx
@@ -1,37 +1,22 @@
 import React from 'react';
-import { Maintainability, Repo, Grade } from './letterGrade.module.scss';
-
-const LetterGrade = props => {
-
-  const getColor = (grade) => {
-    switch(grade) {
-      case "A": return "green"
-      case "B": return "greenyellow"
-      case "C": return "yellow"
-      case "D": return "orange"
-      case "F": return "red"
-      default : return "black"
-    }
-  }
+import { Repo, Grade } from './letterGrade.module.scss';
+const LetterGrade = ({ repo, color }) => {
   return (
-    <>
-      <div className={Maintainability}>
-        {props.ccrepos.map(repo => {
-          const color = getColor(repo.grade)
-          return (
-            <p className={Repo}>
-              {repo.name}:{' '}
-              <span style={{backgroundColor: color, padding: '0px 5px', textAlign: 'center'} }>
-                <a className={Grade} style={{ color: 'white' }}>
-                  {' '}
-                  {repo.grade}
-                </a>
-              </span>
-            </p>
-          );
-        })}
-      </div>
-    </>
+    <p className={Repo}>
+      {repo.name}:{' '}
+      <span
+        style={{
+          backgroundColor: color,
+          padding: '0px 5px',
+          textAlign: 'center',
+        }}
+      >
+        <a className={Grade} style={{ color: 'white' }}>
+          {' '}
+          {repo.grade}
+        </a>
+      </span>
+    </p>
   );
 };
 
