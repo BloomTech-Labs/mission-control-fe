32f283ab5c5c9158b06ae16decbb9f9908d91269
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2020-02-27 14:17:46 -0800

    Commit 2: matched prelim to design given

--- src/components/Project/Grade/index.jsx
+++ src/components/Project/Grade/index.jsx
@@ -2,19 +2,40 @@ import React from 'react';
 // import { useQuery } from 'urql';
 
 import {
-
+  gradeCont,
+  gradeBox,
+  scsssux
 } from './Grade.module.scss';
 
 const Grade = (props) => {
 
     return (
       <>
-        <div>
-          {props.ccrepo.map(repo => (
-            <h2>{repo.name} - {repo.grade}</h2>
-          ))}
+        <div className={gradeCont}>
+          {props.ccrepos.map(repo => {
+            let color = 'white';
+
+            if (repo.grade === 'A') color = 'green';
+            if (repo.grade === 'B') color = 'greenyellow';
+            if (repo.grade === 'C') color = 'yellow';
+            if (repo.grade === 'D') color = 'orange';
+            if (repo.grade === 'F') color = 'red';
+
+            return (
+              <div className={scsssux}>
+              <h3>
+                {repo.name}: {' '}
+                <a className={gradeBox} style={{backgroundColor: color, padding: '0px 6px', borderRadius: '3px'}}>
+                 {' '}
+                 {repo.grade} 
+                </a>
+              </h3>
+              <p>Go to Analysis</p>
+              </div>
+            )
+          })}
         </div>
-        </>
+      </>
     ) 
 };
 
