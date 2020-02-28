5167f8638f203109badc304ecf281fca51328f69
cireimu <ethanhoover3@gmail.com>
2020-02-28 13:32:44 -0800

    added basic update functionality

--- src/components/Settings/StatusLabel.jsx
+++ src/components/Settings/StatusLabel.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
-import {labelDesign} from './Settings.module.scss';
+import { labelDesign, modalStyle } from './Settings.module.scss';
+import { Modal, Header } from 'semantic-ui-react';
 import { FaRegEdit } from 'react-icons/fa';
-
+import UpdateLabel from './UpdateLabel';
 const StatusLabel = props => {
-
-    return (
-        <div>
-            <div
-            className={labelDesign}
-            style={{ background: `${props.label.color}` }}
-            >
-            {props.label.name}
-            </div>
-            <FaRegEdit />
-        </div>
-        
-      );
-}
+  return (
+    <div>
+      <div
+        className={labelDesign}
+        style={{ background: `${props.label.color}` }}
+      >
+        {props.label.name}
+        <Modal trigger={<FaRegEdit />}>
+          <Modal.Header>Edit Label</Modal.Header>
+          <Modal.Content>
+            <Modal.Description className={modalStyle}>
+              <Header>Update Label</Header>
+              <UpdateLabel {...props} label={props.label} />
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
+      </div>
+    </div>
+  );
+};
 
 export default StatusLabel;
