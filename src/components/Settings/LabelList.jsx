da7eccb7192c6a8cd49a4bee7e90ca4ab385bc45
Robbie Porter <rthomporter@gmail.com>
2020-02-27 21:20:39 -0600

    bye bye reactstrap/ hello semantic UI

--- src/components/Settings/LabelList.jsx
+++ src/components/Settings/LabelList.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Button, UncontrolledPopover, PopoverBody } from 'reactstrap';
 import { useQuery } from 'urql';
 import { LABELS_QUERY as query } from '../ProjectList/Queries/projectQueries';
-import { labelDesign } from './Settings.module.scss'
+import {
+  labelDesign,
+  labelListStyle,
+  labelListStyleB,
+} from './Settings.module.scss';
 
 const LabelList = () => {
   const [state] = useQuery({ query, requestPolicy: 'cache-and-network' });
@@ -10,31 +13,22 @@ const LabelList = () => {
   const { data } = state;
 
   return (
-    <div>
-      <Button id="LabelsView" type="button">
-        View Labels
-      </Button>
-      <UncontrolledPopover
-        trigger="legacy"
-        placement="bottom"
-        target="LabelsView"
-      >
-        <PopoverBody>
-          {data
-            ? data.labels.map(label => {
-                return (
-                  <div
-                    className={labelDesign}
-                    style={{ background: `${label.color}` }}
-                    key={label.id}
-                  >
-                    {label.name}
-                  </div>
-                );
-              })
-            : ''}
-        </PopoverBody>
-      </UncontrolledPopover>
+    <div className={labelListStyleB}>
+      <div className={labelListStyle}>
+        {data
+          ? data.labels.map(label => {
+              return (
+                <div
+                  className={labelDesign}
+                  style={{ background: `${label.color}` }}
+                  key={label.id}
+                >
+                  {label.name}
+                </div>
+              );
+            })
+          : ''}
+      </div>
     </div>
   );
 };
