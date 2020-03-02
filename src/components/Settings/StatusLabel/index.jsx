37fad734165dbac1550c98867468ef03820dc4f3
Robbie Porter <rthomporter@gmail.com>
2020-03-02 17:43:37 -0600

    anotha one

--- src/components/Settings/StatusLabel/index.jsx
+++ src/components/Settings/StatusLabel/index.jsx
@@ -3,7 +3,7 @@ import { labelDesign, editModalStyle } from './StatusLabel.module.scss';
 import { Modal, Header } from 'semantic-ui-react';
 import { FaRegEdit } from 'react-icons/fa';
 import UpdateLabel from '../UpdateLabel/index';
-import DeleteLabel from '../DeleteLabel/DeleteLabel';
+import DeleteLabel from '../DeleteLabel/DeleteLabeleLabel';
 const StatusLabel = props => {
   return (
     <div className={labelDesign} style={{ background: `${props.label.color}` }}>
