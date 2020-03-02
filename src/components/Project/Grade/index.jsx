9b6bc981f94e88a28ac42c840d12c8e8649bf07d
Devin Warrick <dev.warr@gmail.com>
2020-03-02 13:41:40 -0600

    imports getColor() function; checks to ensure ccrepos exist before rendering

--- src/components/Project/Grade/index.jsx
+++ src/components/Project/Grade/index.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
-// import { useQuery } from 'urql';
+import getColor from '../../../utils/getColorFromCCGrade';
 
-import {
-  gradeCont,
-  gradeBox,
-  scsssux
-} from './Grade.module.scss';
+import { gradeCont, gradeBox, scsssux } from './Grade.module.scss';
 
-const Grade = (props) => {
-
-    return (
-      <>
-        <div className={gradeCont}>
-          {props.ccrepos.map(repo => {
-            let color = 'black';
-
-            if (repo.grade === 'A') color = 'green';
-            if (repo.grade === 'B') color = 'greenyellow';
-            if (repo.grade === 'C') color = 'yellow';
-            if (repo.grade === 'D') color = 'orange';
-            if (repo.grade === 'F') color = 'red';
-
-            return (
-              <div key={repo.name} className={scsssux}>
-              <h3>
-                {repo.name}: {' '}
-                <a href={repo.link} className={gradeBox} style={{backgroundColor: color, padding: '0px 6px', borderRadius: '3px'}}>
-                 {' '}
-                 {repo.grade} 
-                </a>
-              </h3>
-              <a href={repo.link}>Go to Analysis</a>
-              </div>
-            )
-          })}
-        </div>
-      </>
-    ) 
+const Grade = props => {
+  if (!props.ccrepos || !props.ccrepos.length) return null;
+  
+  return (
+    <div className={gradeCont}>
+      {props.ccrepos.map(repo => {
+        const color = getColor(repo.grade);
+        return (
+          <div key={repo.name} className={scsssux}>
+            <h3>
+              {repo.name}:{' '}
+              <a
+                href={repo.link}
+                className={gradeBox}
+                style={{
+                  backgroundColor: color,
+                  padding: '0px 6px',
+                  borderRadius: '3px',
+                }}
+              >
+                {' '}
+                {repo.grade}
+              </a>
+            </h3>
+            <a href={repo.link}>Go to Analysis</a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Grade;
