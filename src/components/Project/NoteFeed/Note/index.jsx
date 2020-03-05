c251e17fb1d29c3d46f0465d1bc2ef639811a144
newbi462 <isthisemailfree@gmail.com>
2020-03-05 17:25:40 -0500

    ID found

--- src/components/Project/NoteFeed/Note/index.jsx
+++ src/components/Project/NoteFeed/Note/index.jsx
@@ -64,7 +64,7 @@ const Note = ({ note, user, projectManagers, projectId }) => {
               starDimension="20px"
               starSpacing=".5px"
             />
-            <button>TOGGLE</button>
+            <button>{note.id}</button>
           </div>
           <div className={noteBody}>{content}</div>
         </div>
