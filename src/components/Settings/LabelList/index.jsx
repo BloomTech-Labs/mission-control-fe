a3a8f85fc0e8f98f34e5d174b03bf054bc0855a8
cireimu <ethanhoover3@gmail.com>
2020-03-06 16:11:31 -0800

    update create index to clear input state after save

--- src/components/Settings/LabelList/index.jsx
+++ src/components/Settings/LabelList/index.jsx
@@ -5,8 +5,7 @@ import {
   LABELS_SUBSCRIPTION,
   LABEL_LIST_VIEW as query,
 } from '../../ProjectList/Queries/projectQueries';
-import { useSubscription, useQuery } from 'urql';
-import { LabelContext } from '../../../contexts/LabelContext';
+import { useQuery } from 'urql';
 
 const LabelList = ({ column }) => {
   const [state] = useQuery({
@@ -33,32 +32,6 @@ const LabelList = ({ column }) => {
         : ''}
     </div>
   );
-
-  // Testing out subscriptions with labels, to no success.
-  // Originally attempted to just use labelSubscription, but wouldn't render anything
-  // so I followed how they had it setup for the howtographql tutorial
-  // If you want to test it out on the project_list_view stuff,
-  // you will first need to s et up the sub on the backend, then go to /middleware/Urql.jsx
-  // and swap LABELS_QUERY for the other one, as well as make the sub for the front end
-  // const { labelArray } = useContext(LabelContext);
-  // const handleLabelSubscription = response => {
-  //   return [response.newLabels, ...labelArray];
-  // };
-  // console.log(handleLabelSubscription);
-
-  // const [state] = useQuery({ query, requestPolicy: 'cache-and-network' });
-  // useSubscription({ query: LABELS_SUBSCRIPTION }, handleLabelSubscription);
-  // const { data } = state;
-  // console.log('LABEL DATA', data);
-  // return (
-  //   <div className={labelListStyle}>
-  //     {data
-  //       ? data.labels.map(label => {
-  //           return <StatusLabel label={label} key={label.id} />;
-  //         })
-  //       : ''}
-  //   </div>
-  // );
 };
 
 export default LabelList;
