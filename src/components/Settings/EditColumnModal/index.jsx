c2e280809ff6eb02c65b826b2277e40550a93f83
cireimu <ethanhoover3@gmail.com>
2020-03-09 09:17:46 -0700

    started update column

--- src/components/Settings/EditColumnModal/index.jsx
+++ src/components/Settings/EditColumnModal/index.jsx
@@ -3,14 +3,15 @@ import { Button, Modal } from 'semantic-ui-react';
 import EditIcon from '@material-ui/icons/Edit';
 import { LabelContext } from '../../../contexts/LabelContext';
 import { modalStyle, buttonStyle } from './EditColumnModal.module.scss';
-
+import { useMutation } from 'urql';
 import CreateLabel from '../CreateLabel/index';
 import LabelList from '../LabelList/index';
+import { UPDATE_COLUMN } from '../../Project/Queries/index';
 
 const EditColumnModal = ({ column }) => {
-  const { setLabel } = useContext(LabelContext);
-
+  const [updateColumn, setUpdateColumn] = useState({ name: column.name });
   const [open, setOpen] = useState(false);
+  const [, executeMutation] = useMutation(UPDATE_COLUMN);
 
   const handleOpen = () => setOpen(true);
 
@@ -18,7 +19,14 @@ const EditColumnModal = ({ column }) => {
 
   const toggle = () => {
     handleClose();
-    setLabel({ id: '', color: '', name: '' });
+    setUpdateColumn({ id: '', color: '', name: '' });
+  };
+  const handleChanges = e => {
+    e.preventDefault();
+    setUpdateColumn({
+      ...updateColumn,
+      [e.target.name]: e.target.value,
+    });
   };
 
   return (
@@ -35,8 +43,8 @@ const EditColumnModal = ({ column }) => {
     >
       <Modal.Header>Edit Column</Modal.Header>
       <Modal.Content>
+        <input name="name" value={updateColumn.name} onChange={handleChanges} />
         <Modal.Description>
-          <CreateLabel column={column} />
           <LabelList column={column} />
         </Modal.Description>
       </Modal.Content>
