28e224087a0215d02d6a83317c244668b1de549a
Lexie Jiang <lexiemingjiang@gmail.com>
2020-03-10 09:35:02 -0700

    rePulse set up

--- src/components/Project/rePulse/Pulse.jsx
+++ src/components/Project/rePulse/Pulse.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { useQuery } from 'urql';
+import { PULSE as query } from '../Queries/pulseQuery';
+import PulseChart from './Charts/PulseChart';
 
-const rePulse = () => {
+const rePulse = ({ owner, name }) => {
     
+    const [state] = useQuery({ query, variables: {
+        owner: owner,
+        name: name
+    }});
+
+    const { data } = state;
+
+    if (state.fetching){
+        return <p>Detecting Pulse...</p>
+    } else if (state.error) {
+        return <p>No pulse detected __________________ : {state.error}</p>
+    } else if (data.GithubPulse.length) {
+        return (
+            <div className={PulseBoy}>
+                <PulseChart data={data} />
+            </div>
+        );
+    } else {
+        return (
+            <p>Pulse not detected. Deceased.</p>
+        )
+    }
 };
 
 export default rePulse;
