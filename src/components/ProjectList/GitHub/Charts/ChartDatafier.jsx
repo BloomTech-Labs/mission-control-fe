54187da19cd8fe3a42586e7feb6315512dbd5710
Robert Gordon <Komeuppance@gmail.com>
2020-03-09 21:28:42 -0700

    Adjust maxValueCheck logic

--- src/components/ProjectList/GitHub/Charts/ChartDatafier.jsx
+++ src/components/ProjectList/GitHub/Charts/ChartDatafier.jsx
@@ -1,25 +1,18 @@
+const maxValueCheck = (array, value, maxValue) =>{
+    if (value > maxValue ){
+        array.push(maxValue);
+    } else {
+        array.push(value);
+    };
+};
+
 export const ChartDatafier = (data, additions, deletions, changedFiles) => {
     const maxValue = 1000
     data.SparkyBoy.reverse().map(commit => {
-        if (commit.additions > maxValue ){
-            additions.push(maxValue)
-        } else {
-            additions.push(commit.additions)
-        }
-        if (commit.deletions > maxValue ){
-            deletions.push(maxValue)
-        } else {
-            deletions.push(commit.deletions)
-        }
-        if (commit.changedFiles > maxValue ){
-            changedFiles.push(maxValue)
-        } else {
-            changedFiles.push(commit.changedFiles)
-        }
         return (
-            additions,
-            deletions,
-            changedFiles
-        )
+            maxValueCheck(additions, commit.additions, maxValue),
+            maxValueCheck(deletions, commit.deletions, maxValue),
+            maxValueCheck(changedFiles, commit.changedFiles, maxValue)
+        );
     });
-}
+};
