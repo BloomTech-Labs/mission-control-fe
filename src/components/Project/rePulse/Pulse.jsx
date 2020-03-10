f77b31a7fbb9573234dba83f3557f7fb3f21718d
Lexie Jiang <lexiemingjiang@gmail.com>
2020-03-10 10:27:58 -0700

    Barchart for Pulse rendered success, styled appropriately
    Co-authored-by: Robert <GordonRobertD@gmail.com>

--- src/components/Project/rePulse/Pulse.jsx
+++ src/components/Project/rePulse/Pulse.jsx
@@ -3,7 +3,7 @@ import { useQuery } from 'urql';
 import { PULSE as query } from '../Queries/pulseQuery';
 import PulseChart from './Charts/PulseChart';
 
-const rePulse = ({ owner, name }) => {
+const RePulse = ({ owner, name }) => {
     
     const [state] = useQuery({ query, variables: {
         owner: owner,
@@ -12,14 +12,16 @@ const rePulse = ({ owner, name }) => {
 
     const { data } = state;
 
+    console.log(data, "PulseChart");
+
     if (state.fetching){
         return <p>Detecting Pulse...</p>
     } else if (state.error) {
         return <p>No pulse detected __________________ : {state.error}</p>
-    } else if (data.GithubPulse.length) {
+    } else if (data.GithubPulse) {
         return (
-            <div className={PulseBoy}>
-                <PulseChart data={data} />
+            <div>
+                <PulseChart data={data.GithubPulse} />
             </div>
         );
     } else {
@@ -29,4 +31,4 @@ const rePulse = ({ owner, name }) => {
     }
 };
 
-export default rePulse;
+export default RePulse;
