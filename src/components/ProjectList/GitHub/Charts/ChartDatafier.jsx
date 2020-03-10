885879d9d4cf93dfff98f8c0a5381c9232ecd2ca
Robert Gordon <Komeuppance@gmail.com>
2020-03-09 21:13:25 -0700

    Set max value of data set to 1000

--- src/components/ProjectList/GitHub/Charts/ChartDatafier.jsx
+++ src/components/ProjectList/GitHub/Charts/ChartDatafier.jsx
@@ -1,3 +1,25 @@
-export const ChartDatafier = () => {
-    
+export const ChartDatafier = (data, additions, deletions, changedFiles) => {
+    const maxValue = 1000
+    data.SparkyBoy.reverse().map(commit => {
+        if (commit.additions > maxValue ){
+            additions.push(maxValue)
+        } else {
+            additions.push(commit.additions)
+        }
+        if (commit.deletions > maxValue ){
+            deletions.push(maxValue)
+        } else {
+            deletions.push(commit.deletions)
+        }
+        if (commit.changedFiles > maxValue ){
+            changedFiles.push(maxValue)
+        } else {
+            changedFiles.push(commit.changedFiles)
+        }
+        return (
+            additions,
+            deletions,
+            changedFiles
+        )
+    });
 }
