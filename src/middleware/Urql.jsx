0d64d91d37f780913d36c3e0a2da3c29a96bd1c7
cireimu <ethanhoover3@gmail.com>
2020-03-12 16:28:29 -0700

    took out subscriptions and subscription related dependency

--- src/middleware/Urql.jsx
+++ src/middleware/Urql.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Provider,
-  Client,
-  dedupExchange,
-  fetchExchange,
-  subscriptionExchange,
-} from 'urql';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
+import { Provider, Client, dedupExchange, fetchExchange } from 'urql';
 import { cacheExchange } from '@urql/exchange-graphcache';
 import { getToken } from '../utils';
 import {
@@ -74,41 +67,12 @@ const cache = cacheExchange({
         });
       },
     },
-    Subscription: {
-      newLabel: ({ newLabel }, _args, cache) => {
-        cache.updateQuery({ query: PROJECT_LIST_VIEW }, data => {
-          if (data !== null) {
-            data.labels.unshift(newLabel);
-            data.labels.count++;
-            return data;
-          } else {
-            return null;
-          }
-        });
-      },
-    },
-  },
-});
-const subscriptionClient = new SubscriptionClient('ws://localhost:7000', {
-  reconnect: true,
-  connectionParams: () => {
-    const token = getToken();
-    return {
-      headers: { authorization: `Bearer ${token}` },
-    };
   },
 });
 
 const client = new Client({
   url: `${process.env.REACT_APP_URQL_URL}`,
-  exchanges: [
-    dedupExchange,
-    cache,
-    fetchExchange,
-    subscriptionExchange({
-      forwardSubscription: operation => subscriptionClient.request(operation),
-    }),
-  ],
+  exchanges: [dedupExchange, cache, fetchExchange],
   fetchOptions: () => {
     const token = getToken();
     return {
