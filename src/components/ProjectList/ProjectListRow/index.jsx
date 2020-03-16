f828916b643b71d2cbd72d9dd10d7710075956f6
cireimu <ethanhoover3@gmail.com>
2020-03-16 13:27:10 -0700

    merged from master

--- src/components/ProjectList/ProjectListRow/index.jsx
+++ src/components/ProjectList/ProjectListRow/index.jsx
@@ -20,7 +20,6 @@ const ProjectRow = ({ project, statusColumn }) => {
           <Link to={`/project/${project.id}`} className={title}>
             {project.name}
           </Link>
-          <LetterGradeContainer ccrepos={project.product.grades} />
         </td>
         {/* We want to use this feature, but it's not quite where we want it to be just yet. Since we'd have to rip it out in multiple files, I decided to leave it in and comment it out for now. See ProjectListContainer */}
         {/* <td className={rtd}>
