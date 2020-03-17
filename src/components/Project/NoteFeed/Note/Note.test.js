52e12be303ae5892b8ef2d554052ab6b3e91eee0
Code4Blessings <robinwarden@code4blessings.com>
2020-03-17 18:50:21 -0400

    Created Note.test.js file that tests for what's displayed

---
+++ src/components/Project/NoteFeed/Note/Note.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render, fireEvent } from "@testing-library/react";
+import Index from './index';
+
+//Are notes displaying?
+test('Do notes display', () => {
+    expect(true).toBe(true)
+})
+
+//Does 'Public' display on notes?
+test('Public displays on note', () => {
+    expect(true).toBe(true)
+})
+
+//Does 'Private' display on notes?
+test('Private displays on notes', () => {
+    expect(true).toBe(true)
+})
+
+//Do the 'ratings' display on notes?
+test('Ratings display on notes', () => {
+    expect(true).toBe(true)
+})
+
+//Do the attendees appear on the notes
+
+//At student login does 'Public' display
+
+//At student login does 'Private' display
+
+//At manager login does 'Public' display
+
+//At manager login does 'Private' display
+
+
+
