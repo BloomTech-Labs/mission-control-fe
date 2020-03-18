13aeb25a604b102486fd1fd701fd7dd7d1d9c418
Robert Gordon <Komeuppance@gmail.com>
2020-03-18 10:00:05 -0700

    Fixed getMessageForCCGrade to get that sweet CC green checkmark

--- src/utils/getMessageForCCGrade.js
+++ src/utils/getMessageForCCGrade.js
@@ -1,31 +1,22 @@
 import React from 'react';
+
 const getMessage = grade => {
   switch (grade) {
     case 'A':
       return <>Projects with Technical Debt Ratios below 5% are rated A</>;
     case 'B':
-      return (
-        <>Projects with Technical Debt Ratios between 5% and 10% are rated B</>
-      );
+      return <>Projects with Technical Debt Ratios between 5% and 10% are rated B</>;
     case 'C':
-      return (
-        <>Projects with Technical Debt Ratios between 10% and 20% are rated C</>
-      );
+      return <>Projects with Technical Debt Ratios between 10% and 20% are rated C</>;
     case 'D':
-      return (
-        <>Projects with Technical Debt Ratios between 20% and 50% are rated D</>
-      );
+      return <>Projects with Technical Debt Ratios between 20% and 50% are rated D</>;
     case 'F':
       return <>Projects with Technical Debt Ratios above 50% are rated F</>;
     case 'N':
       return <>This repo hasn't received a Maintainability Rating.</>;
     case '!':
-      return (
-        <>
-          This repo hasn't had a successful analysis for its default branch yet.
-        </>
-      );
-  }
+      return <>This repo hasn't had a successful analysis for its default branch yet.</>;
+  };
 };
 
 export default getMessage;
