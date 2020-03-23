23015dc898bc6c9fd8facd4de99aeb4d5dfb9f7c
Robbie Porter <rthomporter@gmail.com>
2020-03-23 12:01:44 -0500

    Reverted past two commits to fix breaking code

--- src/components/ProjectList/LabelDropdown/index.jsx
+++ src/components/ProjectList/LabelDropdown/index.jsx
@@ -6,6 +6,7 @@ import {
   DISCONNECT_SELECTED_LABEL,
 } from '../../Project/Queries/index';
 import { useMutation } from 'urql';
+//test
 
 const LabelDropdown = ({ labels, project }) => {
   const [, executeUpdate] = useMutation(UPDATE_SELECTED_LABEL);
