e4de0eb5df21d5e9659271979603199a64ef64b1
Robert Gordon <Komeuppance@gmail.com>
2020-03-23 09:28:13 -0700

    Adjust formatting

--- src/components/ProjectList/GitHub/Sparkline.jsx
+++ src/components/ProjectList/GitHub/Sparkline.jsx
@@ -5,32 +5,34 @@ import { Sparkyline } from './Sparkline.module.scss';
 import SparkyChart from './Charts/SparkyChart';
 
 const Sparkline = ({ name, repoIndex, handleSparkClick }) => {
+  const [state] = useQuery({
+    query,
+    variables: {
+      owner: 'Lambda-School-Labs',
+      name: name,
+    },
+  });
+  const { data } = state;
 
-    const [state] = useQuery({
-        query, variables: {
-            owner: 'Lambda-School-Labs',
-            name: name
-        }
-    });
-    const { data } = state;
-
-    if (state.fetching) {
-        return <p>Loading Sparkline...</p>
-    } else if (state.error) {
-        return <p>Error: Sparkline unavailable.</p>
-    } else if (data.SparkyBoy.length) {
-        return (
-            <>
-                <div className={Sparkyline} onClick={() => handleSparkClick(repoIndex)}>
-                    <SparkyChart data={data} tooltips={{ enabled: false }} maxValue={1000} />
-                </div>
-            </>
-        );
-    } else {
-        return (
-            <p>Sparkline unavailable</p>
-        )
-    }
+  if (state.fetching) {
+    return <p>Loading Sparkline...</p>;
+  } else if (state.error) {
+    return <p>Error: Sparkline unavailable.</p>;
+  } else if (data.SparkyBoy.length) {
+    return (
+      <>
+        <div className={Sparkyline} onClick={() => handleSparkClick(repoIndex)}>
+          <SparkyChart
+            data={data}
+            tooltips={{ enabled: false }}
+            maxValue={1000}
+          />
+        </div>
+      </>
+    );
+  } else {
+    return <p>Sparkline unavailable</p>;
+  }
 };
 
 export default Sparkline;
