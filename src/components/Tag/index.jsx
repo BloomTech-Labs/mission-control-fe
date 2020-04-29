5246ef1b462fae51706e98a2b35cde4af1e473d8
unknown <ksemenza13@gmail.com>
2020-04-29 13:12:28 -0400

    changed tag card view location

--- src/components/Tag/index.jsx
+++ src/components/Tag/index.jsx
@@ -1,9 +1,7 @@
 //Lab23-T1
-import React, { useEffect, useState, Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'urql';
 import Header from '../Tag/Header';
-import { Popconfirm } from 'antd';
-import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons'
 
 //Styling
 import {
@@ -21,10 +19,10 @@ import {
 
 
 const Tag = props => {
-  const { id, name, handleOnClick, handleOnEdit, handleOnDelete, isUsed } = props.match.params;
+  const { id } = props.match.params;
 
   //LAB23-T1  executeQuery will be used to query other obj arrays
-  const [state] = useQuery({
+  const [state, executeQuery] = useQuery({
     query: tagViewQuery,
     variables: { id },
   });
@@ -47,68 +45,13 @@ const Tag = props => {
   return (
     <div className={parentTagContainer}>
       <div className={tagPageContents}>
-          <Header 
-          tagId={id} 
-          extra={
-            <Fragment>
-              <span className='pointer'
-              onClick={() => 
-              handleOnEdit({id, name, isUsed})}>
+        <div>
+          <Header tagId={id} />
+        </div>
 
-              <EditOutlined
-              style={{
-                fontSize: `1.25rem`,
-                color: '#08c',
-                marginRight: '0.625rem'
-              }}
-              type='edit'/>
-       </span>
-
-       <Popconfirm
-            title="Are you sure delete this tag?"
-            onConfirm={() =>
-              handleOnDelete({
-                id,
-                name
-              })
-            }
-            okText="Yes"
-            cancelText="No"
-          >
-            <span className='pointer'>
-              <DeleteOutlined
-              style={{
-                fontSize:'1.25rem',
-                color:'#08c',
-                marginRight:'0.625rem'
-              }}
-              type='delete'
-              />
-            </span>
-            </Popconfirm>
-            <span className="pointer">
-            <EyeOutlined
-              style={{
-                fontSize: '1.25rem',
-                color: '#08c'
-              }}
-              type="eye"
-              onClick={() => handleOnClick(id)}
-            />
-          </span>
-            </Fragment>
-          } style={{
-            marginBottom:'0.625rem'
-          }}
-          >
-           
-    </Header>
       </div>
     </div>
-
   );
 };
 
 export default Tag
-
-
