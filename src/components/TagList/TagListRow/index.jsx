e81570d8e2bc739f1585f5364c3616deb9d52a3a
unknown <ksemenza13@gmail.com>
2020-04-29 13:53:22 -0400

    crud view implemented in tag list row

--- src/components/TagList/TagListRow/index.jsx
+++ src/components/TagList/TagListRow/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Fragment} from 'react';
 import { Link } from 'react-router-dom';
+
+import { Popconfirm } from 'antd';
+import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons'
 
 import {
   title,
 
 } from './tagListRow.module.scss';
 
-const TagRow = ({ tag, statusColumn }) => {
+const TagRow = ({ tag, content, id, name, handleOnClick, handleOnEdit, handleOnDelete, isUsed, statusColumn }) => {
   const TagsArr = [];
 
   // Display status indicators if present
@@ -20,6 +23,61 @@ const TagRow = ({ tag, statusColumn }) => {
         <td className={title}>
           <Link to={`/tag/${tag.id}`} className={title}>
             {tag.name}
+            extra={
+              <Fragment>
+                <span className='pointer'
+                onClick={() => 
+                handleOnEdit({id, name, isUsed})}>
+  
+                <EditOutlined
+                style={{
+                  fontSize: `1.25rem`,
+                  color: '#08c',
+                  marginRight: '0.625rem'
+                }}
+                type='edit'/>
+         </span>
+  
+         <Popconfirm
+              title="Are you sure delete this tag?"
+              onConfirm={() =>
+                handleOnDelete({
+                  id,
+                  name
+                })
+              }
+              okText="Yes"
+              cancelText="No"
+            >
+              <span className='pointer'>
+                <DeleteOutlined
+                style={{
+                  fontSize:'1.25rem',
+                  color:'#08c',
+                  marginRight:'0.625rem'
+                }}
+                type='delete'
+                />
+              </span>
+              </Popconfirm>
+              <span className="pointer">
+              <EyeOutlined
+                style={{
+                  fontSize: '1.25rem',
+                  color: '#08c'
+                }}
+                type="eye"
+                onClick={() => handleOnClick(id)}
+              />
+            </span>
+              </Fragment>
+            } style={{
+              marginBottom:'0.625rem'
+            }}
+
+          >
+            {content}
+
           </Link>
         </td>
         {statusColumn.length > 0 && TagsArr.length > 0
--- src/components/TagList/index.jsx
+++ src/components/TagList/index.jsx
@@ -1,21 +1,17 @@
 //LAB23-T1
-import React, { Fragment } from 'react';
+import React from 'react';
 import { useQuery } from 'urql';
 import { TAG_LIST_VIEW as query } from './Queries/tagQueries';
 import TagListContainer from './TagListContainer';
 import TagListRow from './TagListRow';
 import Settings from '../Settings/Settings'
 
-import { Popconfirm } from 'antd';
-import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons'
-
 
 // TagListView is the default view when a user signs into the application
 // The TAG_LIST_VIEW query matches against the currently authenticated user
 // and returns a list of tags that they are authorized to view.
 
-const TagListView = ({ content, id, name, handleOnClick, handleOnEdit, handleOnDelete, isUsed }) => {
-
+const TagListView = () => {
   const [state] = useQuery({ query });
   const { data, fetching, error } = state;
   if (error) {
@@ -40,62 +36,7 @@ console.log(`TAG_DATA:%0`, data)
           <TagListRow
             key={tag.id}
             tag={tag}
-
-            extra={
-              <Fragment>
-                <span className='pointer'
-                onClick={() => 
-                handleOnEdit({id, name, isUsed})}>
-  
-                <EditOutlined
-                style={{
-                  fontSize: `1.25rem`,
-                  color: '#08c',
-                  marginRight: '0.625rem'
-                }}
-                type='edit'/>
-         </span>
-  
-         <Popconfirm
-              title="Are you sure delete this tag?"
-              onConfirm={() =>
-                handleOnDelete({
-                  id,
-                  name
-                })
-              }
-              okText="Yes"
-              cancelText="No"
-            >
-              <span className='pointer'>
-                <DeleteOutlined
-                style={{
-                  fontSize:'1.25rem',
-                  color:'#08c',
-                  marginRight:'0.625rem'
-                }}
-                type='delete'
-                />
-              </span>
-              </Popconfirm>
-              <span className="pointer">
-              <EyeOutlined
-                style={{
-                  fontSize: '1.25rem',
-                  color: '#08c'
-                }}
-                type="eye"
-                onClick={() => handleOnClick(id)}
-              />
-            </span>
-              </Fragment>
-            } style={{
-              marginBottom:'0.625rem'
-            }}
-
-          >
-            {content}
-            </TagListRow>
+          />
           
         )) 
         }
